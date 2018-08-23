1ab4f067f94d253cdb7499aecfce489b8147f380
Walker <philwalker@wks57872.bauer-uk.bauermedia.group>
2018-08-23 15:55:12 +0100

    initial upload, post access check added by myself

---
+++ Work Scripts (Jamf Pro)/Allow Standard User Access/Enable access to Date and Time preferences/Enable standard user access to Date and Time preferences.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Grant standard users access to Date and Time preferences
+
+# If the original files are already existing then apply the changes
+
+	if [[ -d "/usr/local/DateTime_Prefs/" ]]; then
+
+		security authorizationdb write system.preferences.datetime allow
+
+else
+
+# Copy the original DateTime preferences files to a root folder and then apply the changes
+
+	if [[ ! -d "/usr/local/DateTime_Prefs/" ]]; then
+
+		mkdir /usr/local/DateTime_Prefs
+
+		security authorizationdb read system.preferences.datetime > /usr/local/DateTime_Prefs/system.preferences.datetime
+		security authorizationdb write system.preferences.datetime allow
+
+	fi
+
+fi
+
+
+USER_AUTH=$(/usr/libexec/PlistBuddy -c "print authenticate-user" /usr/local/DateTime_Prefs/system.preferences.datetime)
+
+if [[ $USER_AUTH == "true" ]]; then
+
+	echo "Standard user granted access to Date & Time preferences"
+
+else
+
+	echo "Setting access to Date & Time preferences failed"
+	exit 1
+
+fi
+
+exit 0
---
+++ Work Scripts (Jamf Pro)/Allow Standard User Access/Enable access to network preferences/Enable standard user access to network preferences.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Grant standard users access to network preferences
+
+# If the original files are already existing then apply the changes
+
+	if [[ -d "/usr/local/Network_Prefs/" ]]; then
+
+		security authorizationdb write system.preferences.network allow
+		security authorizationdb write system.services.systemconfiguration.network allow
+
+else
+
+# Copy the original network preferences files to a root folder and then apply the changes
+
+	if [[ ! -d "/usr/local/Network_Prefs/" ]]; then
+
+		mkdir /usr/local/Network_Prefs
+
+		security authorizationdb read system.preferences.network > /usr/local/Network_Prefs/system.preferences.network
+		security authorizationdb read system.services.systemconfiguration.network > /usr/local/Network_Prefs/system.services.systemconfiguration.network
+
+		security authorizationdb write system.preferences.network allow
+		security authorizationdb write system.services.systemconfiguration.network allow
+
+	fi
+
+fi
+
+
+USER_AUTH_PREF=$(/usr/libexec/PlistBuddy -c "print authenticate-user" /usr/local/Network_Prefs/system.preferences.network)
+USER_AUTH_SERV=$(/usr/libexec/PlistBuddy -c "print authenticate-user" /usr/local/Network_Prefs/system.services.systemconfiguration.network)
+
+if [[ $USER_AUTH_PREF == "true" ]] && [[ $USER_AUTH_SERV == "true" ]]; then
+
+	echo "Standard user granted access to Network preferences"
+
+else
+
+	echo "Setting access to Network preferences failed"
+	exit 1
+
+fi
+
+exit 0
