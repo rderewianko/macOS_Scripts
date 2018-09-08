29f585802043ebd0d7cf23009145c1fea0ced488
pwalker1485 <pwalker1485@icloud.com>
2018-09-09 00:37:29 +0100

    commands all now run as the logged in user to enable script to be run as root

--- System Configuration/FinderPreferences.sh
+++ System Configuration/FinderPreferences.sh
@@ -1,29 +1,32 @@
 #!/bin/sh
 
 #######################################################
-################# Set Finder Preferences ##############
-############# Script created by Phil Walker ###########
+#     Set Finder Preferences for the logged in user   #
+#             Script created by Phil Walker           #
 #######################################################
 
 # Set Finder Preferences
 
+# Get the logged in user
+LoggedInUser=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+
 echo setting Finder preferences...
 
 # Display Disks/Drives/Servers on Desktop
-defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
-defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+su $LoggedInUser -c 'defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true'
+su $LoggedInUser -c 'defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true'
+su $LoggedInUser -c 'defaults write com.apple.finder ShowMountedServersOnDesktop -bool true'
+su $LoggedInUser -c 'defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true'
 
 # Show Status Bar
-defaults write com.apple.finder ShowStatusBar -bool true
+su $LoggedInUser -c 'defaults write com.apple.finder ShowStatusBar -bool true'
 
 # Show Path Bar
-defaults write com.apple.finder ShowPathbar -bool true
+su $LoggedInUser -c 'defaults write com.apple.finder ShowPathbar -bool true'
 
 # Show Side Bar
-defaults write com.apple.finder ShowSidebar -bool true
-defaults write com.apple.finder ShowSideBar -bool true
+su $LoggedInUser -c 'defaults write com.apple.finder ShowSidebar -bool true'
+su $LoggedInUser -c 'defaults write com.apple.finder ShowSideBar -bool true'
 
 # Restart Finder
 killall Finder
