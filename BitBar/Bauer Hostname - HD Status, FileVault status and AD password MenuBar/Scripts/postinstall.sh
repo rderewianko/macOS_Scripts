ac4d7cc8de9683cf98211cfff36d869786f909d0
pwalker1485 <pwalker1485@icloud.com>
2018-09-16 00:42:28 +0100

    reworded comment

--- BitBar/Bauer Hostname - HD Status, FileVault status and AD password MenuBar/Scripts/postinstall.sh
+++ BitBar/Bauer Hostname - HD Status, FileVault status and AD password MenuBar/Scripts/postinstall.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-## Load the launch agent now so the uploader works after pacakge install.
+#!/bin/bash
+## Load and start the launch agent
 
 launchctl load /Library/LaunchAgents/com.hostname.menubar.plist
 launchctl start /Library/LaunchAgents/com.hostname.menubar.plist
