2655dced1698cfd504de5412b7941cf8a4a1ceed
Walker <philwalker@wks57872.bauer-uk.bauermedia.group>
2018-09-17 11:06:56 +0100

    Added additional URL

--- BitBar/JSS_Switcher.365d.sh
+++ BitBar/JSS_Switcher.365d.sh
@@ -6,10 +6,10 @@ LoggedInUser=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyCons
 #Get the current JSS url from the JAMf plist and add to a variable
 jssURL=$(defaults read /Users/$LoggedInUser/Library/Preferences/com.jamfsoftware.jss.plist url)
 #Check which URL is configured and set the name that appears in the menu bar
-if [[ $jssURL == "https://casper.bauerservices.co.uk:443" ]] || [[ $jssURL == "https://casper.bauerservices.co.uk" ]] ; then
+if [[ $jssURL == "https://casper.bauerservices.co.uk:443/" ]] || [[ $jssURL == "https://casper.bauerservices.co.uk/" ]] || [[ $jssURL == "https://casper.bauerservices.co.uk" ]] ; then
 	echo "Prod JSS | color=blue"
 fi
-if [[ $jssURL == "https://caspertest.bauerservices.co.uk:443" ]] || [[ $jssURL == "https://caspertest.bauerservices.co.uk" ]] ; then
+if [[ $jssURL == "https://caspertest.bauerservices.co.uk:443/" ]] || [[ $jssURL == "https://caspertest.bauerservices.co.uk/" ]] || [[ $jssURL == "https://caspertest.bauerservices.co.uk" ]] ; then
 	echo "Test JSS | color=red"
 fi
 
