0ab3bd3cb8cb1992b8247f90196fcd7a42bf16cf
pwalker1485 <pwalker1485@icloud.com>
2018-12-08 13:47:04 +0000

    initial upload

---
+++ Work Scripts (Jamf Pro)/OneDrive/Set One Drive to Insider Ring/OneDriveInsiderRing.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+########################################################################
+#       Set OneDrive to Insiders Rings for the logged in user          #
+################ Written by Phil Walker Dec 2018 #######################
+########################################################################
+
+########################################################################
+#                            Variables                                 #
+########################################################################
+
+#Get the logged in user
+LoggedInUser=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+
+#Get the logged in user group ID
+LoggedInUserGroupID=$(dscl . read /Users/$LoggedInUser | grep "PrimaryGroupID" | awk '{print $2}')
+echo "DEBUG : $LoggedInUserGroupID"
+
+if [ "$LoggedInUserGroupID" -eq "1116074086" ]; then
+  LoggedInUserGroup="BAUER-UK\Domain Users"
+else
+  LoggedInUserGroup="staff"
+fi
+
+########################################################################
+#                         Script starts here                           #
+########################################################################
+
+echo "Setting OneDrive to via the Insiders Ring"
+Defaults write /Users/$LoggedInUser/Library/Preferences/com.microsoft.OneDriveUpdater Tier Insiders
+chown "$LoggedInUser":"$LoggedInUserGroupID" /Users/$LoggedInUser/Library/Preferences/com.microsoft.OneDriveUpdater.plist
+chmod 600 /Users/$LoggedInUser/Library/Preferences/com.microsoft.OneDriveUpdater.plist
+echo "Restarting Defaults Cache"
+killall cfprefsd
+echo "Refreshing Update Rings"
+rm /Users/$LoggedInUser/Library/Caches/OneDrive/UpdateRingSettings.json
+echo "All set!"
+
+exit 0
