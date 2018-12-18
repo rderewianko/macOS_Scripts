1fa7706060ea9a749cbd7e85f027af98f4655d47
Phil Walker <pwalker1485@icloud.com>
2018-12-18 10:55:13 +0000

    update variables
    Updated variables to use $() instead of ``

--- Work Scripts (Jamf Pro)/Current_User_Directory_Size.sh
+++ Work Scripts (Jamf Pro)/Current_User_Directory_Size.sh
@@ -8,13 +8,13 @@
 # Locating the last logged in user.
 #lastUser=`defaults read /Library/Preferences/com.apple.loginwindow lastUserName`
 
-LoggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+LoggedInUser=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
 
 # Calculating last Logged in user's Home Folder Size.
-userHomeFolderSize=`du -sk /Users/$LoggedInUser/ | awk '{ print $1 }'`
+userHomeFolderSize=$(du -sk /Users/$LoggedInUser/ | awk '{ print $1 }')
 
 # Calculating last logged in user's Outlook Database Size
-OutlookProfile=`du -sk /Users/$LoggedInUser/Library/Group\ Containers/UBF8T346G9.Office/Outlook/Outlook\ 15\ Profiles | awk '{ print $1 }'`
+OutlookProfile=$(du -sk /Users/$LoggedInUser/Library/Group\ Containers/UBF8T346G9.Office/Outlook/Outlook\ 15\ Profiles | awk '{ print $1 }')
 
 # Subtracting out Outlook Profile Size
 totalSize=$(($userHomeFolderSize - $OutlookProfile))
