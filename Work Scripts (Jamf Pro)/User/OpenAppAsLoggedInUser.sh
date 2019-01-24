2665e56b509b9f06b1a1d64e85996db64f6ac877
pwalker1485 <pwalker1485@icloud.com>
2019-01-24 14:13:15 +0000

    amended command to run as user

--- Work Scripts (Jamf Pro)/User/OpenAppAsLoggedInUser.sh
+++ Work Scripts (Jamf Pro)/User/OpenAppAsLoggedInUser.sh
@@ -7,7 +7,7 @@
 
 # Amend variable assignment for Application as required
 
-LoggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+LoggedInUser=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
 
 App="Softphone"
 
