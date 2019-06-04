b6e878ee3cebb778e694cb6451e021a9fcfd2b20
Phil Walker <philwalker@wks57984.med.bauer-uk.bauermedia.group>
2019-06-04 15:17:31 +0100

    initial upload

---
+++ Work Scripts (Jamf Pro)/Power Management/Power_Management_Sleep_Settings.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+########################################################################
+#          Amend power management settings to allow MacBooks to        #
+#                       enter a full sleep state                       #
+################## Written by Phil Walker June 2019 ####################
+########################################################################
+
+# Get hibernate mode status
+hibernateMode=$(pmset -g | grep "hibernatemode" | awk '{ print $2 }')
+# Get standby mode status
+standbyStatus=$(pmset -g | grep -v "standbydelayhigh\|standbydelaylow\|highstandbythreshold" | grep "standby" | awk '{ print $2 }')
+# Get standbydelaylow status
+standbydelaylowStatus=$(pmset -g | grep "standbydelaylow" | awk '{ print $2 }')
+# Get standbydelayhigh status
+standbydelayhighStatus=$(pmset -g | grep "standbydelayhigh" | awk '{ print $2 }')
+
+echo "Power Management Settings"
+echo "hibernatemode = $hibernateMode"
+echo "standby = $standbyStatus"
+echo "standbydelaylowStatus = $standbydelaylowStatus"
+echo "standbydelayhighStatus = $standbydelayhighStatus"
+echo ""
+
+echo "Checking if changes are required..."
+if [[ "$hibernateMode" -ne "25" ]];then
+  echo "Changing hibernatemode to 25"
+  pmset -a hibernatemode 25
+else
+  echo "hibernatemode already set to 25, nothing to do"
+fi
+
+if [[ "$standbyStatus" -ne "1" ]];then
+  echo "Changing standby to 1"
+  pmset -a standby 1
+else
+  echo "standby already set to 1, nothing to do"
+fi
+
+if [[ "$standbydelaylowStatus" -ne "600" ]];then
+  echo "Changing standbydelaylow to 10 minutes"
+  pmset -a standbydelaylow 600
+else
+  echo "standbydelaylow already set to 10 minutes, nothing to do"
+fi
+
+if [[ "$standbydelayhighStatus" -ne "600" ]];then
+  echo "Changing standbydelayhigh to 10 minutes"
+  pmset -a standbydelayhigh 600
+else
+  echo "standbydelayhigh already set to 10 minutes, nothing to do"
+fi
+
+# re-populate all variables
+hibernateMode=$(pmset -g | grep "hibernatemode" | awk '{ print $2 }')
+standbyStatus=$(pmset -g | grep -v "standbydelayhigh\|standbydelaylow\|highstandbythreshold" | grep "standby" | awk '{ print $2 }')
+standbydelaylowStatus=$(pmset -g | grep "standbydelaylow" | awk '{ print $2 }')
+standbydelayhighStatus=$(pmset -g | grep "standbydelayhigh" | awk '{ print $2 }')
+
+echo ""
+echo "Power Management Settings"
+echo "hibernatemode = $hibernateMode"
+echo "standby = $standbyStatus"
+echo "standbydelaylowStatus = $standbydelaylowStatus"
+echo "standbydelayhighStatus = $standbydelayhighStatus"
+
+exit 0
