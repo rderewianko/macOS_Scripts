f18d65222f0e9aedb8a10230e4419578dd91a099
Phil Walker <philwalker@wks57984.med.bauer-uk.bauermedia.group>
2019-06-24 12:55:06 +0100

    renamed variable

--- Work Scripts (Jamf Pro)/Extension Attributes/Oracle_JDK_8_Previous_Versions.sh
+++ Work Scripts (Jamf Pro)/Extension Attributes/Oracle_JDK_8_Previous_Versions.sh
@@ -5,9 +5,9 @@
 ################## Written by Phil Walker June 2019 ####################
 ########################################################################
 
-jdk8Check=$(ls /Library/Java/JavaVirtualMachines/ | grep "1.8" 2>/dev/null | awk 'END { print NR }')
+jdk8_check=$(ls /Library/Java/JavaVirtualMachines/ | grep "1.8" 2>/dev/null | awk 'END { print NR }')
 
-if [[ "$jdk8Check" -gt "1" ]]; then
+if [[ "$jdk8_check" -gt "1" ]]; then
 
   jdk8_current=$(/usr/libexec/java_home | cut -d '/' -f5 | sed -e 's/jdk//' -e 's/.jdk//')
   jdk8_previous=$(ls /Library/Java/JavaVirtualMachines/ | grep "jdk1.8" | sed -e 's/jdk//' -e 's/.jdk//' | grep -v "$jdk8_current" > /var/tmp/jdk8_previous_versions.txt)
