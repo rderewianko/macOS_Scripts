df8bcf85b0c4ee8bbe700135aff0fdb67792aec9
Phil Walker <philwalker@wks57984.med.bauer-uk.bauermedia.group>
2019-08-01 16:45:35 +0100

    amended BTDevices2 to excluded iPhone-Wirel

--- Work Scripts (Jamf Pro)/Extension Attributes/Connected Bluetooth Devices.sh
+++ Work Scripts (Jamf Pro)/Extension Attributes/Connected Bluetooth Devices.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-########################################################################
-#                  Connected Bluetooth Devices EA                      #
-############### written by Suleyman Twana and Phil Walker ##############
-########################################################################
-
 loggedInUser=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
 mobileAccount=$(dscl . read /Users/${loggedInUser} OriginalNodeName 2>/dev/null)
 
@@ -15,7 +10,7 @@ else
 fi
 
 BTDevices1=$(system_profiler SPBluetoothDataType > /tmp/BT.txt)
-BTDevices2=$(cat /tmp/BT.txt | grep -i "$userName" | grep -vi "wireless" | sed 's/://g' | sed -e '/^ *$/d')
+BTDevices2=$(cat /tmp/BT.txt | grep -i "$userName" | grep -vi "wireless" | grep -vi "iPhone-Wirel" | sed 's/://g' | sed -e '/^ *$/d')
   if [[ $loggedInUser == "root" ]]; then
     exit 0
   else
