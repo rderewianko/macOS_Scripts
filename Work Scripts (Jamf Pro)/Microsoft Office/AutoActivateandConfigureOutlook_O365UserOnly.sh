f34d1f469ddf9bcf19b5825ce0dd1001ee6f84e3
Phil Walker <philwalker@wks57984.med.bauer-uk.bauermedia.group>
2019-10-05 22:58:33 +0100

    added domain ping

--- Work Scripts (Jamf Pro)/Microsoft Office/AutoActivateandConfigureOutlook_O365UserOnly.sh
+++ Work Scripts (Jamf Pro)/Microsoft Office/AutoActivateandConfigureOutlook_O365UserOnly.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ########################################################################
-#      Auto activate Office 2019 and auto configure Outlook 2019       #
+#  Auto activate Office 2019/365 and auto configure Outlook 2019/365   #
 ################# Written by Phil Walker August 2019 ###################
 ########################################################################
 
@@ -21,10 +21,8 @@ officeVersion=$(/usr/libexec/PlistBuddy -c 'Print CFBundleShortVersionString' /A
 userEmail=$(dscl /Active\ Directory/BAUER-UK/bauer-uk.bauermedia.group -read /Users/$loggedInUser | grep EMailAddress: | awk '{print $2}')
 #Get the logged in users UPN
 userUPN=$(dscl /Active\ Directory/BAUER-UK/bauer-uk.bauermedia.group -read /Users/$loggedInUser | grep "userPrincipalName" | awk '{print $2}')
-#Office plist
-officePlist="/Users/$loggedInUser/Library/Preferences/com.microsoft.office.plist"
-#Outlook plist
-outlookPlist="/Users/$loggedInUser/Library/Preferences/com.microsoft.Outlook.plist"
+#Domain
+theDomain="bauer-uk.bauermedia.group"
 
 ########################################################################
 #                         Script starts here                           #
@@ -35,15 +33,29 @@ if [[ "$loggedInUser" == "" ]] || [[ "$loggedInUser" == "root" ]]; then
   exit 0
 fi
 
+#First check if we can get to AD
+domainPing=$(ping -c1 -W5 -q bauer-uk.bauermedia.group 2>/dev/null | head -n1 | sed 's/.*(\(.*\))/\1/;s/:.*//')
+if [[ "$domainPing" == "" ]]; then
+  echo "$theDomain is not reachable so auto config and activation cannot complete"
+  exit 0
+else
+  echo "$theDomain is reachable, continuing..."
+fi
+
 #If there is a logged in user, confirm that Office 2019 is installed before continuing
 if [[ "$officeVersion" -ge "1617" ]]; then
   echo "Office 2019 installed, checking if $loggedInUser is an Office 365 user..."
       if [[ "$mailboxValue" == "-1073741818" ]]; then
         echo "$loggedInUser is an Office 365 user, auto activating Office and configuring Outlook..."
 
-          su -l "$loggedInUser" -c "defaults write "$officePlist" OfficeActivationEmailAddress -string "$userUPN""
-          su -l "$loggedInUser" -c "defaults write "$officePlist" OfficeAutoSignIn -bool TRUE"
-          su -l "$loggedInUser" -c "defaults write "$outlookPlist" DefaultEmailAddressOrDomain -string "$userEmail""
+          su -l "$loggedInUser" -c "defaults write com.microsoft.office OfficeActivationEmailAddress -string "$userUPN""
+          su -l "$loggedInUser" -c "defaults write com.microsoft.office OfficeAutoSignIn -bool TRUE"
+          su -l "$loggedInUser" -c "defaults write com.microsoft.Outlook DefaultEmailAddressOrDomain -string "$userEmail""
+          su -l "$loggedInUser" -c "defaults write com.microsoft.Outlook kSubUIAppCompletedFirstRunSetup1507 -bool FALSE"
+          su -l "$loggedInUser" -c "defaults write com.microsoft.Word kSubUIAppCompletedFirstRunSetup1507 -bool FALSE"
+          su -l "$loggedInUser" -c "defaults write com.microsoft.Excel kSubUIAppCompletedFirstRunSetup1507 -bool FALSE"
+          su -l "$loggedInUser" -c "defaults write com.microsoft.onenote.mac kSubUIAppCompletedFirstRunSetup1507 -bool FALSE"
+          su -l "$loggedInUser" -c "defaults write com.microsoft.Powerpoint kSubUIAppCompletedFirstRunSetup1507 -bool FALSE"
 
       else
         echo "$loggedInUser is an On-Premises user, nothing to do"
@@ -52,21 +64,29 @@ if [[ "$officeVersion" -ge "1617" ]]; then
       fi
 fi
 
+#Confirm the first run reset has been completed successfully
+OutlookFirstRun=$(su -l "$loggedInUser" -c "defaults read com.microsoft.Outlook kSubUIAppCompletedFirstRunSetup1507")
+if [[ "$OutlookFirstRun" == "0" ]] || [[ "$OutlookFirstRun" == "false" ]]; then
+  echo "First run status reset"
+else
+  echo "First run completed, Office may need to be manually activated"
+fi
+
 #Confirm the changes have been made successfully
-if [[ $(su -l "$loggedInUser" -c "defaults read "$officePlist" OfficeActivationEmailAddress") == "$userUPN" ]]; then
+if [[ $(su -l "$loggedInUser" -c "defaults read com.microsoft.office OfficeActivationEmailAddress") == "$userUPN" ]]; then
   echo "Office activation email set to $userUPN"
 else
   echo "Office activation email not set"
 fi
 
-officeAutoSignIn=$(su -l "$loggedInUser" -c "defaults read "$officePlist" OfficeAutoSignIn")
-if [[ "$officeAutoSignIn" -eq "1" ]] || [[ "$officeAutoSignIn" == "true" ]]; then
+officeAutoSignIn=$(su -l "$loggedInUser" -c "defaults read com.microsoft.office OfficeAutoSignIn")
+if [[ "$officeAutoSignIn" == "1" ]] || [[ "$officeAutoSignIn" == "true" ]]; then
   echo "Office auto sign in enabled"
 else
   echo "Office auto sign not enabled"
 fi
 
-if [[ $(su -l "$loggedInUser" -c "defaults read "$outlookPlist" DefaultEmailAddressOrDomain -string "$userEmail"") == "$userEmail" ]]; then
+if [[ $(su -l "$loggedInUser" -c "defaults read com.microsoft.Outlook DefaultEmailAddressOrDomain -string "$userEmail"") == "$userEmail" ]]; then
   echo "Outlook default email address set to $userEmail"
 else
   echo "Outlook default email address not set"
