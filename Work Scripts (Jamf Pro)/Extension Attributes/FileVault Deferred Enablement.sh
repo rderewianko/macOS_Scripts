8833bf840696938ec1d86dc277101d0a24832206
Phil Walker <philwalker@wks70010.med.bauer-uk.bauermedia.group>
2020-01-13 14:11:30 +0000

    added exclusion for Catalina

--- Work Scripts (Jamf Pro)/Extension Attributes/FileVault Deferred Enablement.sh
+++ Work Scripts (Jamf Pro)/Extension Attributes/FileVault Deferred Enablement.sh
@@ -5,16 +5,23 @@
 ################## written by Phil Walker July 2019 ####################
 ########################################################################
 
+#Get OS version
+osShort=$(sw_vers -productVersion | awk -F. '{print $2}')
+#FileVault deferral status
 FV2Deferred=$(fdesetup status | sed -n 2p)
 
-if [[ "$FV2Deferred" == "" ]] || [[ "$FV2Deferred" =~ "Encryption in progress" ]]; then
-  echo "<result>Not deferred</result>"
+if [[ "$osShort" -ge "15" ]]; then
+  echo "<result>N/A</result>"
 else
-  FV2DeferralUser=$(fdesetup status | sed -n 2p | awk '{print $9}' | cut -d "'" -f2)
-  if [[ "$FV2DeferralUser" == "" ]]; then
-    echo "<result>FileVault deferred</result>"
+  if [[ "$FV2Deferred" == "" ]] || [[ "$FV2Deferred" =~ "Encryption in progress" ]]; then
+    echo "<result>Not deferred</result>"
   else
-    echo "<result>FileVault deferred for: ${FV2DeferralUser}</result>"
+    FV2DeferralUser=$(fdesetup status | sed -n 2p | awk '{print $9}' | cut -d "'" -f2)
+    if [[ "$FV2DeferralUser" == "" ]]; then
+      echo "<result>FileVault deferred</result>"
+    else
+      echo "<result>FileVault deferred for: ${FV2DeferralUser}</result>"
+    fi
   fi
 fi
 
