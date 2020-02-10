f44c2719e6a826afdb5426d6ae6bcf187c248dea
Phil Walker <pwalker1485@icloud.com>
2020-02-10 20:51:26 +0000

    reverted back to stat command for logged in user

--- Work Scripts (Jamf Pro)/Temp Admin Rights/New package/Scripts/Disable logged in user Admins.sh
+++ Work Scripts (Jamf Pro)/Temp Admin Rights/New package/Scripts/Disable logged in user Admins.sh
@@ -10,7 +10,7 @@
 ########################################################################
 
 #Get the logged in user
-loggedInUser=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+loggedInUser=$(stat -f %Su /dev/console)
 
 #Get the hostname
 hostName=$(scutil --get HostName)
