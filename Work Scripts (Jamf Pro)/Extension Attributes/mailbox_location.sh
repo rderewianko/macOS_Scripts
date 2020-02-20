8e3f055fbd9f27594bad861108dcf8d46cd17195
Phil Walker <pwalker1485@icloud.com>
2020-02-20 13:51:54 +0000

    added additional value for O365 mailbox

--- Work Scripts (Jamf Pro)/Extension Attributes/mailbox_location.sh
+++ Work Scripts (Jamf Pro)/Extension Attributes/mailbox_location.sh
@@ -4,31 +4,27 @@
 #This Extension Attribute finds if the logged in user in 365 or on premise
 ##########################################################################
 
-#Set the AD attribute number for key msExchRecipientDisplayType that correspond to where a mailbox is hosted.
-user365="-1073741818"
-userOnPRem="1073741824"
-
 #Get the logged in user
-LoggedInUser=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+loggedInUser=$(stat -f %Su /dev/console)
 
 # If nobody's home do nothing.
-if [ "$LoggedInUser" == "" ]; then
+if [[ "$loggedInUser" == "" ]] || [[ "$loggedInUser" == "root" ]]; then
   echo "<result>No logged in user</result>"
   exit 0
 else
-  #User logged in carry on but check if we can get to AD first
+  #User logged in carry on but check if we can get to AD
   domainPing=$(ping -c1 -W5 -q bauer-uk.bauermedia.group 2>/dev/null | head -n1 | sed 's/.*(\(.*\))/\1/;s/:.*//')
   if [[ "$domainPing" == "" ]]; then
     echo "<result>Domain not reachable</result>"
     exit 0
   fi
   #Get the value of msExchRecipientDisplayType
-  mailboxValue=$(dscl /Active\ Directory/BAUER-UK/bauer-uk.bauermedia.group -read /Users/$LoggedInUser | grep "msExchRecipientDisplayType" | awk '{print$2}')
+  mailboxValue=$(dscl /Active\ Directory/BAUER-UK/bauer-uk.bauermedia.group -read /Users/$loggedInUser | grep "msExchRecipientDisplayType" | awk '{print$2}')
 
   #Check the value from AD against the known values for mailbox location and echo back the result
-  if [[ "$mailboxValue" == "$user365" ]]; then
+  if [[ "$mailboxValue" == "-1073741818" ]] || [[ "$mailboxValue" == "-2147483642" ]]; then
     echo "<result>365 Mailbox</result>"
-  elif [[ "$mailboxValue" == "$userOnPRem" ]]; then
+  elif [[ "$mailboxValue" == "1073741824" ]]; then
     echo "<result>On Premise Mailbox</result>"
   else
     echo "<result>Mailbox details not found</result>"
