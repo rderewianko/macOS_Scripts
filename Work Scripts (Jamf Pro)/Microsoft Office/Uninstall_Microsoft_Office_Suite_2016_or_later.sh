31a70ca9daf8e79a885160ecd0461745fde76481
Phil Walker <pwalker1485@icloud.com>
2020-02-21 11:10:06 +0000

    updating to add removal of additional prefs

--- Work Scripts (Jamf Pro)/Microsoft Office/Uninstall_Microsoft_Office_Suite_2016_or_later.sh
+++ Work Scripts (Jamf Pro)/Microsoft Office/Uninstall_Microsoft_Office_Suite_2016_or_later.sh
@@ -2,26 +2,32 @@
 
 ########################################################################
 #  Uninstall Microsoft Office 2016/2019/365 Suite and all preferences  #
-#################### Written by Phil Walker August #####################
+################### Written by Phil Walker Feb 2020 ####################
 ########################################################################
 
 ########################################################################
-#                             Functions                                #
+#                            Functions                                 #
 ########################################################################
 
 function removeMAU ()
 {
-#Only remove MAU if no other app is dependent on it for updates
+# Only remove MAU if no other app is dependent on it for updates
 mauApps=$(ls /Applications/ | grep -i "Microsoft" | grep -i "Edge\|Remote\|Defender\|Portal")
+
 if [[ "$mauApps" != "" ]]; then
-	echo "Microsoft Auto Update required by other applications so will not be removed"
+
+	echo "MAU required by other applications so will not be removed"
+
 else
-	echo "Removing Microsoft Auto Update..."
-	rm -rf /Library/Application\ Support/Microsoft/MAU2.0
-	rm -f /Library/LaunchAgents/com.microsoft.update.agent.plist
-	rm -f /Library/Preferences/com.microsoft.autoupdate2.plist
-	pkgutil --forget com.microsoft.package.Microsoft_AutoUpdate.app
-	pkgutil --forget com.microsoft.package.Microsoft_AU_Bootstrapper.app
+
+	echo "Removing MAU components..."
+
+		rm -rf /Library/Application\ Support/Microsoft/MAU2.0
+		rm -f /Library/LaunchAgents/com.microsoft.update.agent.plist
+		rm -f /Library/Preferences/com.microsoft.autoupdate2.plist
+		pkgutil --forget com.microsoft.package.Microsoft_AutoUpdate.app 2>/dev/null
+		pkgutil --forget com.microsoft.package.Microsoft_AU_Bootstrapper.app 2>/dev/null
+
 fi
 }
 
@@ -33,15 +39,15 @@ fi
 
 if [[ -d /Applications/Microsoft\ Word.app/ ]]; then
 
+	echo "Removing Office applications..."
+
 		rm -rf "/Applications/Microsoft Excel.app"
 		rm -rf "/Applications/Microsoft OneNote.app"
-    rm -rf "/Applications/Microsoft Outlook.app"
-    rm -rf "/Applications/Microsoft PowerPoint.app"
-    rm -rf "/Applications/Microsoft Word.app"
+  	rm -rf "/Applications/Microsoft Outlook.app"
+  	rm -rf "/Applications/Microsoft PowerPoint.app"
+  	rm -rf "/Applications/Microsoft Word.app"
 		rm -rf "/Applications/OneDrive.app"
 		rm -rf "/Applications/Microsoft Teams.app"
-
-    	echo "Office 2016/2019/365 apps removed"
 
 else
 
@@ -49,29 +55,31 @@ else
 
 fi
 
+removeMAU
+
 # Remove Office 2016 volume license file
 
-	if [ -e "/Library/Preferences/com.microsoft.office.licensingV2.plist" ]; then
+if [ -e "/Library/Preferences/com.microsoft.office.licensingV2.plist" ]; then
 
 	rm -f /Library/Preferences/com.microsoft.office.licensingV2.plist
 
-    	echo "Office 2016 volume license file removed"
+		echo "Office 2016 volume license file removed"
 
 else
 
-    	echo "Office 2016 volume license file not found"
+    echo "Office 2016 volume license file not found"
 
 fi
 
 # Make some temp files to hold the user lists
 
-tmp_users=`mktemp "/tmp/users-unsort-XXXXX"`
+tmp_users=$(mktemp "/tmp/users-unsort-XXXXX")
 
-	if [[ -z "$tmp_users" ]]; then
+if [[ -z "$tmp_users" ]]; then
 
-    	echo "error: could not make tmp file"
+	echo "error: could not make tmp file"
 
-exit 1
+	exit 1
 
 fi
 
@@ -79,9 +87,9 @@ fi
 
 for the_folder in "$targetVolume/Users"; do
 
-    if [[ -d "$the_folder" ]]; then
+	if [[ -d "$the_folder" ]]; then
 
-    ls -lTn "$the_folder" | awk -v vDIR="$the_folder" '/^d/{vUID = $3; vGID = $4; sub("^.*[0-9]+:[0-9]+.[0-9]+","");sub("^ +","");if($0 !~ "^[.]"){print vUID, vGID, vDIR "/" $0}}'
+  ls -lTn "$the_folder" | awk -v vDIR="$the_folder" '/^d/{vUID = $3; vGID = $4; sub("^.*[0-9]+:[0-9]+.[0-9]+","");sub("^ +","");if($0 !~ "^[.]"){print vUID, vGID, vDIR "/" $0}}'
 
 fi
 
@@ -91,97 +99,95 @@ done | sed '/\/Shared/d' >> "$tmp_users"
 
 cat "$tmp_users" | while read the_user; do
 
-    user_uid=`echo "$the_user" | awk '{print $1}'`
-    user_gid=`echo "$the_user" | awk '{print $2}'`
-    user_home=`echo "$the_user" | awk '{$1 = ""; $2 = ""; sub("^ +","");print}'`
+	user_home=$(echo "$the_user" | awk '{$1 = ""; $2 = ""; sub("^ +","");print}')
+	user_name=$(echo "$the_user" | awk '{$1 = ""; $2 = ""; sub("^ +","");print}' | cut -c8-)
 
 # Remove users' Library files
 
-    if [[ -n "$user_home" ]]; then
+  	if [[ -n "$user_home" ]]; then
+
+			echo "Cleaning all Microsoft Office preferences for ${user_name}"
+
+# Only remove MAU preferences if no other app is dependent on it for updates
+		mauApps=$(ls /Applications/ | grep -i "Microsoft" | grep -i "Edge\|Remote\|Defender\|Portal")
+
+		if [[ "$mauApps" == "" ]]; then
+
+			rm -f $user_home/Library/Preferences/com.microsoft.autoupdate.fba.plist 2>/dev/null
+			rm -f $user_home/Library/Preferences/com.microsoft.autoupdate2.plist 2>/dev/null
+			rm -rf $user_home/Library/Application\ Support/Microsoft\ AutoUpdate 2>/dev/null
+			rm -rf $user_home/Library/Application\ Support/Microsoft\ AU\ Daemon 2>/dev/null
+
+		fi
 
     if [[ -d $user_home/Library/Containers/ ]]; then
 
-		rm -rf /$user_home/Library/Containers/com.microsoft.errorreporting
-		rm -rf /$user_home/Library/Containers/com.microsoft.Excel
-		rm -rf /$user_home/Library/Containers/com.microsoft.openxml.excel.app
-		rm -rf /$user_home/Library/Containers/com.microsoft.netlib.shipassertprocess
-		rm -rf /$user_home/Library/Containers/com.microsoft.Office365ServiceV2
-    rm -rf /$user_home/Library/Containers/com.microsoft.OneDrive.FinderSync
-		rm -rf /$user_home/Library/Containers/com.Microsoft.OsfWebHost
-		rm -rf /$user_home/Library/Containers/com.microsoft.Outlook
-		rm -rf /$user_home/Library/Containers/com.microsoft.Powerpoint
-		rm -rf /$user_home/Library/Containers/com.microsoft.RMS-XPCService
-		rm -rf /$user_home/Library/Containers/com.microsoft.SkypeForBusiness
-		rm -rf /$user_home/Library/Containers/com.microsoft.Word
-		rm -rf /$user_home/Library/Containers/com.microsoft.onenote.mac
+			rm -rf $user_home/Library/Containers/com.microsoft.errorreporting 2>/dev/null
+			rm -rf $user_home/Library/Containers/com.microsoft.Excel 2>/dev/null
+			rm -rf $user_home/Library/Containers/com.microsoft.openxml.excel.app 2>/dev/null
+			rm -rf $user_home/Library/Containers/com.microsoft.netlib.shipassertprocess 2>/dev/null
+			rm -rf $user_home/Library/Containers/com.microsoft.Office365ServiceV2 2>/dev/null
+    	rm -rf $user_home/Library/Containers/com.microsoft.OneDrive.FinderSync 2>/dev/null
+			rm -rf $user_home/Library/Containers/com.Microsoft.OsfWebHost 2>/dev/null
+			rm -rf $user_home/Library/Containers/com.microsoft.Outlook 2>/dev/null
+			rm -rf $user_home/Library/Containers/com.microsoft.Powerpoint 2>/dev/null
+			rm -rf $user_home/Library/Containers/com.microsoft.RMS-XPCService 2>/dev/null
+			rm -rf $user_home/Library/Containers/com.microsoft.SkypeForBusiness 2>/dev/null
+			rm -rf $user_home/Library/Containers/com.microsoft.Word 2>/dev/null
+			rm -rf $user_home/Library/Containers/com.microsoft.onenote.mac 2>/dev/null
 
-		rm -rf /$user_home/Library/Cookies/com.microsoft.onedrive.binarycookies
-		rm -rf /$user_home/Library/Cookies/com.microsoft.onedriveupdater.binarycookies
+			rm -rf $user_home/Library/Cookies/com.microsoft.onedrive.binarycookies 2>/dev/null
+			rm -rf $user_home/Library/Cookies/com.microsoft.onedriveupdater.binarycookies 2>/dev/null
 
-		rm -rf /$user_home/Library/Caches/Microsoft
-		rm -rf /$user_home/Library/Caches/OneDrive
-		rm -rf /$user_home/Library/Caches/com.microsoft*
+			rm -rf $user_home/Library/Caches/Microsoft2>/dev/null
+			rm -rf $user_home/Library/Caches/OneDrive 2>/dev/null
+			rm -rf $user_home/Library/Caches/com.microsoft* 2>/dev/null
 
-		rm -rf /$user_home/Library/Application\ Scripts/com.Microsoft.OsfWebHost
-		rm -rf /$user_home/Library/Application\ Scripts/com.microsoft.Powerpoint
-		rm -rf /$user_home/Library/Application\ Scripts/com.microsoft.SkypeForBusiness
-		rm -rf /$user_home/Library/Application\ Scripts/com.microsoft.Word
-		rm -rf /$user_home/Library/Application\ Scripts/com.microsoft.OneDrive.FinderSync
-		rm -rf /$user_home/Library/Application\ Scripts/com.microsoft.onenote.mac
-		rm -rf /$user_home/Library/Application\ Scripts/com.microsoft.Office365ServiceV2
-		rm -rf /$user_home/Library/Application\ Scripts/com.microsoft.Excel
-		rm -rf /$user_home/Library/Application\ Scripts/com.microsoft.openxml.excel.app
-		rm -rf /$user_home/Library/Application\ Scripts/com.microsoft.errorreporting
-		rm -rf /$user_home/Library/Application\ Scripts/com.microsoft.Outlook
+			rm -rf $user_home/Library/Application\ Scripts/com.Microsoft.OsfWebHost 2>/dev/null
+			rm -rf $user_home/Library/Application\ Scripts/com.microsoft.Powerpoint 2>/dev/null
+			rm -rf $user_home/Library/Application\ Scripts/com.microsoft.SkypeForBusiness 2>/dev/null
+			rm -rf $user_home/Library/Application\ Scripts/com.microsoft.Word 2>/dev/null
+			rm -rf $user_home/Library/Application\ Scripts/com.microsoft.OneDrive.FinderSync 2>/dev/null
+			rm -rf $user_home/Library/Application\ Scripts/com.microsoft.onenote.mac 2>/dev/null
+			rm -rf $user_home/Library/Application\ Scripts/com.microsoft.Office365ServiceV2 2>/dev/null
+			rm -rf $user_home/Library/Application\ Scripts/com.microsoft.Excel 2>/dev/null
+			rm -rf $user_home/Library/Application\ Scripts/com.microsoft.openxml.excel.app 2>/dev/null
+			rm -rf $user_home/Library/Application\ Scripts/com.microsoft.errorreporting 2>/dev/null
+			rm -rf $user_home/Library/Application\ Scripts/com.microsoft.Outlook 2>/dev/null
 
-		rm -f /$user_home/Library/Preferences/com.microsoft.autoupdate.fba.plist
-		rm -f /$user_home/Library/Preferences/com.microsoft.onenote.mac.plist
-		rm -f /$user_home/Library/Preferences/com.microsoft.Outlook.plist
-		rm -f /$user_home/Library/Preferences/com.microsoft.autoupdate2.plist
-		rm -f /$user_home/Library/Preferences/com.microsoft.OneDrive.plist
-		rm -f /$user_home/Library/Preferences/com.microsoft.SkypeForBusiness.plist
-		rm -f /$user_home/Library/Preferences/com.microsoft.Excel.plist
-		rm -f /$user_home/Library/Preferences/com.microsoft.office.plist
-		rm -f /$user_home/Library/Preferences/com.microsoft.Powerpoint.plist
-		rm -f /$user_home/Library/Preferences/com.microsoft.Word.plist
-		rm -f /$user_home/Library/Preferences/com.microsoft.OutlookSkypeIntegration.plist
-		rm -f /$user_home/Library/Preferences/com.microsoft.OneDriveUpdater.plist
-		rm -rf /$user_home/Library/Preferences/ByHost/com.microsoft*
+			rm -f $user_home/Library/Preferences/com.microsoft.onenote.mac.plist 2>/dev/null
+			rm -f $user_home/Library/Preferences/com.microsoft.Outlook.plist 2>/dev/null
+			rm -f $user_home/Library/Preferences/com.microsoft.OneDrive.plist 2>/dev/null
+			rm -f $user_home/Library/Preferences/com.microsoft.SkypeForBusiness.plist 2>/dev/null
+			rm -f $user_home/Library/Preferences/com.microsoft.Excel.plist 2>/dev/null
+			rm -f $user_home/Library/Preferences/com.microsoft.office.plist 2>/dev/null
+			rm -f $user_home/Library/Preferences/com.microsoft.Powerpoint.plist 2>/dev/null
+			rm -f $user_home/Library/Preferences/com.microsoft.Word.plist 2>/dev/null
+			rm -f $user_home/Library/Preferences/com.microsoft.OutlookSkypeIntegration.plist 2>/dev/null
+			rm -f $user_home/Library/Preferences/com.microsoft.OneDriveUpdater.plist 2>/dev/null
+			rm -rf $user_home/Library/Preferences/ByHost/com.microsoft* 2>/dev/null
 
-		rm -rf /$user_home/Library/WebKit/com.microsoft.*
+			rm -rf $user_home/Library/WebKit/com.microsoft* 2>/dev/null
 
-		rm -rf /$user_home/Library/Application\ Support/Microsoft\ AutoUpdate
-		rm -rf /$user_home/Library/Application\ Support/com.microsoft.OneDriveUpdater
-		rm -rf /$user_home/Library/Application\ Support/com.microsoft.OneDrive
-		rm -rf /$user_home/Library/Application\ Support/OneDrive
-		rm -rf /$user_home/Library/Application\ Support/OneDriveUpdater
-		rm -rf /$user_home/Library/Application\ Support/Microsoft\ AU\ Daemon
-
-		echo "Users' Library Cleaned"
-
-else
-
-		echo "No Users' Library files found"
+			rm -rf $user_home/Library/Application\ Support/com.microsoft.OneDriveUpdater 2>/dev/null
+			rm -rf $user_home/Library/Application\ Support/com.microsoft.OneDrive 2>/dev/null
+			rm -rf $user_home/Library/Application\ Support/OneDrive 2>/dev/null
+			rm -rf $user_home/Library/Application\ Support/OneDriveUpdater 2>/dev/null
 
 	fi
 
-fi
-
 	if [[ -d $user_home/Library/Group\ Containers/ ]]; then
 
-      rm -rf /$user_home/Library/Group\ Containers/UBF8T346G9.ms
-    	rm -rf /$user_home/Library/Group\ Containers/UBF8T346G9.Office
-			rm -rf /$user_home/Library/Group\ Containers/UBF8T346G9.OfficeOneDriveSyncIntegration
-    	rm -rf /$user_home/Library/Group\ Containers/UBF8T346G9.OfficeOsfWebHost
-			rm -rf /$user_home/Library/Group\ Containers/UBF8T346G9.OneDriveStandaloneSuite
-			rm -rf /$user_home/Library/Group\ Containers/UBF8T346G9.OneDriveSyncClientSuite
+      rm -rf $user_home/Library/Group\ Containers/UBF8T346G9.ms 2>/dev/null
+    	rm -rf $user_home/Library/Group\ Containers/UBF8T346G9.Office 2>/dev/null
+			rm -rf $user_home/Library/Group\ Containers/UBF8T346G9.OfficeOneDriveSyncIntegration 2>/dev/null
+    	rm -rf $user_home/Library/Group\ Containers/UBF8T346G9.OfficeOsfWebHost 2>/dev/null
+			rm -rf $user_home/Library/Group\ Containers/UBF8T346G9.OneDriveStandaloneSuite 2>/dev/null
+			rm -rf $user_home/Library/Group\ Containers/UBF8T346G9.OneDriveSyncClientSuite 2>/dev/null
 
+	fi
 
-        echo "Group Container Cleaned"
-
-else
-
-		echo "No Group Container files found"
+	echo "Microsoft Office preferences cleaned successfully for ${user_name}"
 
 fi
 
@@ -189,43 +195,43 @@ done
 
 # Remove System Library files
 
-			rm -rf /Library/Fonts/Microsoft
-    	rm -f /Library/LaunchDaemons/com.microsoft.office.licensing.helper.plist
-    	rm -f /Library/LaunchDaemons/com.microsoft.office.licensingV2.helper.plist
-			rm -f /Library/LaunchDaemons/com.microsoft.OneDriveUpdaterDaemon.plist
-    	rm -f /Library/Preferences/com.microsoft.Excel.plist
-    	rm -f /Library/Preferences/com.microsoft.office.plist
-    	rm -f /Library/Preferences/com.microsoft.office.setupassistant.plist
-    	rm -f /Library/Preferences/com.microsoft.outlook.databasedaemon.plist
-    	rm -f /Library/Preferences/com.microsoft.outlook.office_reminders.plist
-    	rm -f /Library/Preferences/com.microsoft.Outlook.plist
-    	rm -f /Library/Preferences/com.microsoft.PowerPoint.plist
-    	rm -f /Library/Preferences/com.microsoft.Word.plist
-    	rm -f /Library/PrivilegedHelperTools/com.microsoft.office.licensing.helper
-    	rm -f /Library/PrivilegedHelperTools/com.microsoft.office.licensingV2.helper
+			rm -rf /Library/Fonts/Microsoft 2>/dev/null
+    	rm -f /Library/LaunchDaemons/com.microsoft.office.licensing.helper.plist 2>/dev/null
+    	rm -f /Library/LaunchDaemons/com.microsoft.office.licensingV2.helper.plist 2>/dev/null
+			rm -f /Library/LaunchDaemons/com.microsoft.OneDriveUpdaterDaemon.plist 2>/dev/null
+    	rm -f /Library/Preferences/com.microsoft.Excel.plist 2>/dev/null
+    	rm -f /Library/Preferences/com.microsoft.office.plist 2>/dev/null
+    	rm -f /Library/Preferences/com.microsoft.office.setupassistant.plist 2>/dev/null
+    	rm -f /Library/Preferences/com.microsoft.outlook.databasedaemon.plist 2>/dev/null
+    	rm -f /Library/Preferences/com.microsoft.outlook.office_reminders.plist 2>/dev/null
+    	rm -f /Library/Preferences/com.microsoft.Outlook.plist 2>/dev/null
+    	rm -f /Library/Preferences/com.microsoft.PowerPoint.plist 2>/dev/null
+    	rm -f /Library/Preferences/com.microsoft.Word.plist 2>/dev/null
+    	rm -f /Library/PrivilegedHelperTools/com.microsoft.office.licensing.helper 2>/dev/null
+    	rm -f /Library/PrivilegedHelperTools/com.microsoft.office.licensingV2.helper 2>/dev/null
 
-    	echo "System folders Cleaned"
+    		echo "Microsoft Office System preferences cleaned"
 
-    	echo "Removing Office 2016/2019 package receipts"
+    		echo "Removing Office 2016/2019/365 package receipts..."
 
-			pkgutil --forget com.microsoft.package.Fonts
-			pkgutil --forget com.microsoft.package.DFonts
-			pkgutil --forget com.microsoft.pkg.licensing.volume >/dev/null 2>&1
-			pkgutil --forget com.microsoft.pkg.licensing >/dev/null 2>&1
-			pkgutil --forget com.microsoft.package.Microsoft_Excel.app
-			pkgutil --forget com.microsoft.package.Microsoft_OneNote.app
-			pkgutil --forget com.microsoft.package.Microsoft_Outlook.app
-			pkgutil --forget com.microsoft.package.Microsoft_PowerPoint.app
-			pkgutil --forget com.microsoft.package.Microsoft_Word.app
-			pkgutil --forget com.microsoft.OneDrive
-			pkgutil --forget com.microsoft.SkypeForBusiness
-			pkgutil --forget com.microsoft.package.Proofing_Tools
-			pkgutil --forget com.microsoft.package.licensing
-			pkgutil --forget com.microsoft.pkg.licensing
-			pkgutil --forget com.microsoft.package.Frameworks
-			pkgutil --forget com.microsoft.teams
-			pkgutil --forget microsoftskypeforbusinesssetdefaulttelephony
+			pkgutil --forget com.microsoft.package.Fonts 2>/dev/null
+			pkgutil --forget com.microsoft.package.DFonts 2>/dev/null
+			pkgutil --forget com.microsoft.pkg.licensing.volume 2>/dev/null
+			pkgutil --forget com.microsoft.pkg.licensing 2>/dev/null
+			pkgutil --forget com.microsoft.package.Microsoft_Excel.app 2>/dev/null
+			pkgutil --forget com.microsoft.package.Microsoft_OneNote.app 2>/dev/null
+			pkgutil --forget com.microsoft.package.Microsoft_Outlook.app 2>/dev/null
+			pkgutil --forget com.microsoft.package.Microsoft_PowerPoint.app 2>/dev/null
+			pkgutil --forget com.microsoft.package.Microsoft_Word.app 2>/dev/null
+			pkgutil --forget com.microsoft.OneDrive 2>/dev/null
+			pkgutil --forget com.microsoft.SkypeForBusiness 2>/dev/null
+			pkgutil --forget com.microsoft.package.Proofing_Tools 2>/dev/null
+			pkgutil --forget com.microsoft.package.licensing 2>/dev/null
+			pkgutil --forget com.microsoft.pkg.licensing 2>/dev/null
+			pkgutil --forget com.microsoft.package.Frameworks 2>/dev/null
+			pkgutil --forget com.microsoft.teams 2>/dev/null
+			pkgutil --forget microsoftskypeforbusinesssetdefaulttelephony 2>/dev/null
 
-      echo "All done!"
+      	echo "All done! Microsoft Office has been removed"
 
 exit 0
