d1a597dbfa382794268ceceef705daa2a5737d87
pwalker1485 <pwalker1485@icloud.com>
2020-03-10 11:41:33 +0000

    initial upload

---
+++ Packages/Auto Install Adobe CC Updates/Scripts/preinstall.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+########################################################################
+#    Automatically Install All Adobe CC Application Updates Package    #
+#                         preinstall script                            #                        
+#################### Written by Phil Walker Mar 2020 ###################
+########################################################################
+
+########################################################################
+#                            Variables                                 #
+########################################################################
+
+#Launch Agent
+launchAgent=/Library/LaunchAgents/com.bauer.AdobeRUM.LoginWindow.plist
+#Launch Daemon
+LaunchDaemon=/Library/LaunchDaemons/com.bauer.AdobeRUM.plist
+#Auto update script
+updateScript=/usr/local/bin/AutoInstallAdobeCCUpdates.sh
+
+########################################################################
+#                         Script starts here                           #
+########################################################################
+
+if [[ -e "$launchAgent" || -e "$LaunchDaemon" || -e "$updateScript" ]]; then
+    echo "Previous content found"
+    echo "Removing previous launch agent, daemon and script"
+    rm -f "$launchAgent" 2>/dev/null
+    rm -f "$LaunchDaemon" 2>/dev/null
+    rm -f "$updateScript" 2>/dev/null
+        if [[ ! -e "$launchAgent" && ! -e "$LaunchDaemon" && ! -e "$updateScript" ]]; then
+            echo "Previous content deleted successfully"
+        else
+            echo "clean up FAILED!"
+        fi
+else
+    echo "Previous content not found"
+    echo "Nothing to do"
+fi
+
+exit 0
