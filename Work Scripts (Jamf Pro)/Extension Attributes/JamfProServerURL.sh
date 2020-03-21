51586f7f0d3f04f90fd2d64cd25ba2af5cbcdd5c
pwalker1485 <pwalker1485@icloud.com>
2020-03-21 21:40:30 +0000

    initial upload

---
+++ Work Scripts (Jamf Pro)/Extension Attributes/JamfProServerURL.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+########################################################################
+#           Current Jamf Pro Server URL - Extension Attribute          #
+################## Written by Phil Walker Mar 2020 #####################
+########################################################################
+
+########################################################################
+#                            Variables                                 #
+########################################################################
+
+#JSS URL
+jamfProURL="Your Jamf Pro URL"
+#Get the current JSS url from the Jamf plist
+currentURL=$(defaults read /Library/Preferences/com.jamfsoftware.jamf.plist jss_url)
+
+########################################################################
+#                         Script starts here                           #
+########################################################################
+
+if [[ $jamfProURL =~ "$currentURL" ]]; then 
+    echo "<result>URL Correct</result>"
+else
+    echo "<result>$currentURL</result>"
+fi
+
+exit 0
