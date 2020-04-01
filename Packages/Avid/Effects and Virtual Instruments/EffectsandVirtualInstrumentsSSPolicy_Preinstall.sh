813721f767334ad9d1120807da286db5f890da5b
pwalker1485 <pwalker1485@icloud.com>
2020-04-01 10:54:09 +0100

    initial upload

---
+++ Packages/Avid/Effects and Virtual Instruments/EffectsandVirtualInstrumentsSSPolicy_Preinstall.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+########################################################################
+#         Effects and Virtual Instruments Preinstall Jamf Helper       #
+#################### Written by Phil Walker Jan 2020 ###################
+########################################################################
+
+########################################################################
+#                            Variables                                 #
+########################################################################
+
+#Get the current logged in user and store in variable
+loggedInUser=$(stat -f %Su /dev/console)
+
+########################################################################
+#                            Functions                                 #
+########################################################################
+
+function jamfHelperDownloadInProgress ()
+{
+#Download in progress
+su - $loggedInUser <<'jamfmsg1'
+/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType utility -icon /Library/Application\ Support/JAMF/bin/Management\ Action.app/Contents/Resources/Self\ Service.icns -title "Message from Bauer IT" -heading "Effects and Virtual Instrument Bundle" -alignHeading natural -description "Downloading Effects and Virtual Instruments bundle..." -alignDescription natural &
+jamfmsg1
+}
+
+########################################################################
+#                         Script starts here                           #
+########################################################################
+
+#Show a message via Jamf Helper that the package is being downloaded
+jamfHelperDownloadInProgress
+
+exit 0
