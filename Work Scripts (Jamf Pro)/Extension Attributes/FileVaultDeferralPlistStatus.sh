0face1aec44c49038f470c99a3770b6164d30ce8
pwalker1485 <pwalker1485@icloud.com>
2020-04-06 15:30:10 +0100

    initial upload

---
+++ Work Scripts (Jamf Pro)/Extension Attributes/FileVaultDeferralPlistStatus.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+########################################################################
+#            FileVault Deferral Plist - Extension Attribute            #
+#################### Written by Phil Walker Apr 2020 ###################
+########################################################################
+
+# Deferral plist
+deferralPlist="/usr/local/bin/FileVaultEnablement.plist"
+
+#OS Version Full and Short
+osFull=$(sw_vers -productVersion)
+osShort=$(sw_vers -productVersion | awk -F. '{print $2}')
+
+########################################################################
+#                         Script starts here                           #
+########################################################################
+
+if [[ "$osShort" -ge "15" ]]; then
+    if [[ -e "$deferralPlist" ]]; then
+        echo "<result>Found</result>"
+    else
+        echo "<result>Not Found</result>"
+    fi
+else
+    echo "<result>N/A</result>"
+fi
