e44678e9d555c986518427bd815bfa0dcea51d84
pwalker1485 <pwalker1485@icloud.com>
2020-04-25 21:13:10 +0100

    cleaned up

--- Work Scripts (Jamf Pro)/Google Chrome/Removal_Google_Chrome.sh
+++ Work Scripts (Jamf Pro)/Google Chrome/Removal_Google_Chrome.sh
@@ -12,44 +12,19 @@
 googleChrome="/Applications/Google Chrome.app"
 
 ########################################################################
-#                            Functions                                 #
-########################################################################
-
-function postCheck()
-{
-
-local googleChrome="/Applications/Google Chrome.app"
-
-if [[ ! -d "$googleChrome" ]]; then
-
-  echo "Google Chrome removed successfully"
-  exit 0
-
-else
-
-  echo "Google Chrome removal failed"
-  exit 1
-
-fi
-
-}
-
-########################################################################
 #                         Script starts here                           #
 ########################################################################
 
 if [[ -d "$googleChrome" ]]; then
-
-  echo "Cleaning up old version of Google Chrome..."
-
+  echo "Cleaning up previous version of Google Chrome..."
   rm -rf "$googleChrome"
-
-  postCheck
-
+    if [[ ! -d "$googleChrome" ]]; then
+      echo "Previous version removed successfully"
+    else
+      echo "Prevous version removal failed"
+    fi
 else
   echo "Google Chrome not installed, nothing to clean up"
-  exit 0
-
 fi
 
 exit 0
