0ad69d1c3fc6675179f8ca902193d63fee89acb5
pwalker1485 <pwalker1485@icloud.com>
2020-06-18 21:57:15 +0100

    Changed kill to only InCopy 2017+Added check for succcessful install of 2019 before uninstall

--- Work Scripts (Jamf Pro)/Uninstall Applications/Adobe/Adobe CC Apps/UninstallAdobeInCopy2017.sh
+++ Work Scripts (Jamf Pro)/Uninstall Applications/Adobe/Adobe CC Apps/UninstallAdobeInCopy2017.sh
@@ -1,70 +1,80 @@
-#!/bin/sh
+#!/bin/bash
 
 ########################################################################
 #                    Uninstall Adobe InCopy CC 2017                    #
-#################### Written by Phil Walker Mar 2020 ###################
+#################### Written by Phil Walker June 2020 ##################
 ########################################################################
 
 ########################################################################
 #                            Variables                                 #
 ########################################################################
 
-#path to binary
+# path to binary
 binaryPath="/Library/Application Support/Adobe/Adobe Desktop Common/HDBox/Setup"
-#App sap code
+# App sap code
 sapCode="AICY"
-#App version
+# App version
 appVersion="12.0.0"
+# InCopy 2019 Package receipt
+pkgReceipt=$(pkgutil --pkgs | grep "com.adobe.Enterprise.install.609964F7-F196-4C18-9EE9-7A92585B7A83")
 
 ########################################################################
 #                            Functions                                 #
 ########################################################################
 
-function killAdobe ()
+function killInCopy ()
 {
-#Kill all user Adobe Launch Agents/Daemons
-for pid in $userPID
-    do
-        kill $pid 2>/dev/null
-done
-
-#Unload user Adobe Launch Agents
-su -l "$loggedInUser" -c "/bin/launchctl unload /Library/LaunchAgents/com.adobe.* 2>/dev/null"
-
-#Unload Adobe Launch Daemons
-/bin/launchctl unload /Library/LaunchDaemons/com.adobe.* 2>/dev/null
-
-pkill "obe"
+# InCopy 2017 PID
+incopyPID=$(pgrep "Adobe InCopy CC 2017")
+if [[ "$incopyPID" != "" ]]; then
+    while [[ "$incopyPID" != "" ]]; do
+        kill "$incopyPID" 2>/dev/null
+        sleep 2
+        # re-populate variable
+        incopyPID=$(pgrep "Adobe InCopy CC 2017")
+    done
+    echo "Adobe InCopy CC 2017 process killed"
+else
+    echo "Adobe InCopy CC 2017 not open"
+fi
 }
 
 ########################################################################
 #                         Script starts here                           #
 ########################################################################
 
-if [[ -d /Applications/Adobe\ InCopy\ CC\ 2017 ]]; then
-
-    echo "Adobe InCopy CC 2017 found"
-
-    #Kill all Adobe processes/launch agents/launch daemons
-    killAdobe
-
-    sleep 5
-
-    echo "Uninstalling Adobe InCopy CC 2017..."
-
-    "$binaryPath" --uninstall=1 --sapCode="$sapCode" --baseVersion="$appVersion" --platform=osx10-64 --deleteUserPreferences=false >/dev/null 2>&1
-
-    if [[ $? == "0" ]]; then
-        rm -rf /Applications/Adobe\ InCopy\ CC\ 2017 >/dev/null 2>&1
-        echo "InCopy 2017 uninstalled successfully"
+if [[ "$pkgReceipt" != "" ]] && [[ -d "/Applications/Adobe InCopy CC 2019" ]]; then
+    if [[ -d "/Applications/Adobe InCopy CC 2017" ]]; then
+        echo "Adobe InCopy CC 2017 found"
+        # If open kill the InCopy 2017 process
+        killInCopy
+        sleep 5
+        # Uninstall InCopy 2017
+        echo "Uninstalling Adobe InCopy CC 2017..."
+        "$binaryPath" --uninstall=1 --sapCode="$sapCode" --baseVersion="$appVersion" --platform=osx10-64 --deleteUserPreferences=false >/dev/null 2>&1
+        if [[ "$?" == "0" ]]; then
+            sleep 2
+            # Sometimes the directory is left behind. If found, remove it
+            while [[ -e "/Applications/Adobe InCopy CC 2017" ]]; do
+                echo "Adobe InCopy CC 2017 directory found after uninstall command completed"
+                echo "Deleting Adobe InCopy CC 2017 directory..."
+                rm -rf "/Applications/Adobe InCopy CC 2017" >/dev/null 2>&1
+                sleep 2
+            done
+            # Remove package receipt
+            pkgutil --forget "com.adobe.Enterprise.install.0A83B18F-FB8F-43C8-BD41-8A44297A8FA8" >/dev/null 2>&1
+            echo "Adobe InCopy CC 2017 uninstalled successfully"
+        else
+            echo "Failed to uninstall InCopy CC 2017"
+            exit 1
+        fi
     else
-        echo "Failed to uninstall InCopy CC 2017"
-        exit 1
+        echo "Adobe InCopy CC 2017 not found, nothing to do"
     fi
-
 else
-
-    echo "Adobe InCopy CC 2017 not found, nothing to do"
+    # Package receipt for Adobe InCopy CC 2019 not found so do nothing
+    echo "Adobe InCopy CC 2019 failed to install successfully"
+    echo "Adobe InCopy CC 2017 has not been uninstalled"
 fi
 
 exit 0
