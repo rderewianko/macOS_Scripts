81f86f8de07225a2da70914aae9f8d45f692c47b
pwalker1485 <pwalker1485@icloud.com>
2020-09-17 15:42:30 +0100

    amended for loop to remove admin. removed echo for accounts not touched

--- Work Scripts (Jamf Pro)/FileVault/Remove_Temp_Admin_For_FileVault_Enablement.sh
+++ Work Scripts (Jamf Pro)/FileVault/Remove_Temp_Admin_For_FileVault_Enablement.sh
@@ -9,38 +9,39 @@
 #                            Variables                                 #
 ########################################################################
 
-#Get the logged in user
-loggedInUser=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
-
-#Get a list of users who are in the admin group
+# Get the logged in user
+loggedInUser=$(stat -f %Su /dev/console)
+# Get a list of users who are in the admin group
 adminUsers=$(dscl . -read Groups/admin GroupMembership | cut -c 18-)
 
-#Loop through each account found, excludes root and any account with admin in the name - this stops casadmin, admin and any ADadmin accounts from being removed from the admin group
-for user in $adminUsers
-do
-    if [[ "$user" != "root" && "$user" != *"admin"* ]]; then
-        dseditgroup -o edit -d $user -t user admin
-        if [ $? = 0 ]; then
-          echo "Removed $user from admin group"
+########################################################################
+#                         Script starts here                           #
+########################################################################
+
+echo "Checking for accounts created as admin users for FileVault enablement..."
+# Loop through each account found and remove them from the admin group (excluding root, admin and casadmin)
+for user in $adminUsers; do
+    if [[ "$user" != "root" && "$user" != "admin" && "$user" != "casadmin" ]]; then
+        dseditgroup -o edit -d "$user" -t user admin
+        commandResult="$?"
+        if [[ "$commandResult" -eq "0" ]]; then
+            echo "Removed $user from admin group"
         fi
-    else
-        echo "Admin user $user left alone"
     fi
 done
 
-#Double check that the logged in user has been removed from the admin group
-#re-populate variable
+# Double check that the logged in user has been removed from the admin group
+# re-populate admin group variable
 adminUsers=$(dscl . -read Groups/admin GroupMembership | cut -c 18-)
-if [[ "$adminUsers" =~ "$loggedInUser" ]]; then
-  echo "${loggedInUser} still a member of the admin group, removing admin rights..."
-  dseditgroup -o edit -d $loggedInUser -t user admin
-  #re-populate variable
-  adminUsers=$(dscl . -read Groups/admin GroupMembership | cut -c 18-)
-    if [[ "$adminUsers" =~ "$loggedInUser" ]]; then
-      echo "${loggedInUser} still a member of the admin group"
-      echo "${loggedInUser}'s admin rights must be removed manually"
-      exit 1
+if [[ "$adminUsers" =~ ${loggedInUser} ]]; then
+    echo "${loggedInUser} still a member of the admin group, removing admin rights..."
+    dseditgroup -o edit -d "$loggedInUser" -t user admin
+    # re-populate variable
+    adminUsers=$(dscl . -read Groups/admin GroupMembership | cut -c 18-)
+    if [[ "$adminUsers" =~ ${loggedInUser} ]]; then
+        echo "${loggedInUser} still a member of the admin group"
+        echo "${loggedInUser}'s admin rights must be removed manually"
+        exit 1
     fi
 fi
-
 exit 0
