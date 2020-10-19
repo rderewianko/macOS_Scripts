13b8f5f9da6bca10f5c152055dc39782db9ac9a2
pwalker1485 <pwalker1485@icloud.com>
2020-10-19 14:43:24 +0100

    Added jamf helpers

--- Work Scripts (Jamf Pro)/Adobe/Adobe Acrobat DC/AdobeAcrobatDC_preinstall.sh
+++ Work Scripts (Jamf Pro)/Adobe/Adobe Acrobat DC/AdobeAcrobatDC_preinstall.sh
@@ -1,47 +1,103 @@
 #!/bin/bash
 
 ########################################################################
-# Stop+Unload Adobe Launch Agents/Daemons And Kill All Adobe Processes #
-#################### Written by Phil Walker Mar 2020 ###################
+#         Adobe Acrobat DC Install Policy Script - Preinstall          #
+################### Written by Phil Walker Aug 2020 ####################
 ########################################################################
-
-#Required to avoid new app installation failures
+# Must be set to run before the package install
+# Process:
+# Stop+unload Adobe Launch Agents/Daemons and kill all Adobe processes
 
 ########################################################################
 #                            Variables                                 #
 ########################################################################
 
-#Get the logged in user
+############ Variables for Jamf Pro Parameters - Start #################
+# CC App name for helper windows e.g. Adobe Acrobat DC
+appNameForInstall="$4"
+############ Variables for Jamf Pro Parameters - End ###################
+
+# Get the logged in user
 loggedInUser=$(stat -f %Su /dev/console)
-#Get all user Adobe Launch Agents/Daemons PIDs
-userPIDs=$(su -l "$loggedInUser" -c "/bin/launchctl list | grep adobe" | awk '{print $1}')
+# Jamf Helper
+jamfHelper="/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper"
+if [[ -d "/Applications/Utilities/Adobe Creative Cloud/Utils/Creative Cloud Uninstaller.app" ]]; then
+    # Helper Icon Cloud Uninstaller
+    helperIcon="/Applications/Utilities/Adobe Creative Cloud/Utils/Creative Cloud Uninstaller.app/Contents/Resources/CreativeCloudInstaller.icns"
+else
+    # helper Icon SS
+    helperIcon="/Library/Application Support/JAMF/bin/Management Action.app/Contents/Resources/Self Service.icns"
+fi
+# Helper icon Download
+helperIconDownload="/System/Library/CoreServices/Install in Progress.app/Contents/Resources/Installer.icns"
+# Helper title
+helperTitle="Message from Bauer IT"
+# Helper heading
+helperHeading="          ${appNameForInstall}          "
+
+########################################################################
+#                            Functions                                 #
+########################################################################
+
+function killAdobe ()
+{
+if [[ "$loggedInUser" == "" ]] || [[ "$loggedInUser" == "root" ]]; then
+    echo "No user logged in"
+else
+    # Get all user Adobe Launch Agents/Daemons PIDs
+    userPIDs=$(su -l "$loggedInUser" -c "/bin/launchctl list | grep adobe" | awk '{print $1}')
+    # Kill all user Adobe Launch Agents and Daemons
+    if [[ "$userPIDs" != "" ]]; then
+        while IFS= read -r line; do
+            kill -9 "$line" 2>/dev/null
+        done <<< "$userPIDs"
+    fi
+    # Unload user Adobe Launch Agents
+    su -l "$loggedInUser" -c "/bin/launchctl unload /Library/LaunchAgents/com.adobe.* 2>/dev/null"
+    # Unload Adobe Launch Daemons
+    /bin/launchctl unload /Library/LaunchDaemons/com.adobe.* 2>/dev/null
+    pkill "obe" >/dev/null 2>&1
+    sleep 5
+    # Close any Adobe Crash Reporter windows (e.g. Bridge)
+    pkill -9 "Crash Reporter" >/dev/null 2>&1
+    # Kill Safari processes - can cause install failure (Error DW046 - Conflicting processes are running)
+    killall -9 "Safari" >/dev/null 2>&1
+fi
+}
+
+function jamfHelperCleanUp ()
+{
+# Download in progress helper window
+"$jamfHelper" -windowType utility -icon "$helperIcon" -title "$helperTitle" \
+-heading "$helperHeading" -alignHeading natural -description "Closing all Adobe CC applications and Safari..." -alignDescription natural &
+}
+
+function jamfHelperDownloadInProgress ()
+{
+# Download in progress helper window
+"$jamfHelper" -windowType utility -icon "$helperIconDownload" -title "$helperTitle" \
+-heading "$helperHeading" -alignHeading natural -description "Downloading and Installing ${appNameForInstall}...
+
+⚠️ Please do not open any Adobe CC app ⚠️
+
+** Download time will depend on the speed of your current internet connection **" -alignDescription natural &
+}
 
 ########################################################################
 #                         Script starts here                           #
 ########################################################################
 
-#Kill all user Adobe Launch Agents and Daemons
-if [[ "$userPIDs" != "" ]]; then
-    while IFS= read -r line; do
-        kill -9 "$line" 2>/dev/null
-    done <<< "$userPIDs"
-fi
-echo "All user Adobe Launch Agent and Daemon processes killed"
-
-#Unload user Adobe Launch Agents
-su -l "$loggedInUser" -c "/bin/launchctl unload /Library/LaunchAgents/com.adobe.* 2>/dev/null"
-echo "All user Adobe Launch Agents unloaded"
-
-#Unload Adobe Launch Daemons
-/bin/launchctl unload /Library/LaunchDaemons/com.adobe.* 2>/dev/null
-echo "All Adobe Launch Daemons unloaded"
-
-pkill "obe"
+# jamf Helper for killing apps and uninstalling previous versions
+jamfHelperCleanUp
+# Wait a few seconds for the helper message to be seen before closing the apps
 sleep 5
-#Close any Adobe Crash Reporter windows (e.g. Bridge)
-pkill -9 "Crash Reporter"
-echo "All remaining Adobe processes killed"
-# Kill Safari processes - can cause install failure (Error DW046 - Conflicting processes are running)
-killall -9 "Safari"
+# Kill processes to allow uninstall
+killAdobe
+# Wait before uninstalling
+sleep 10
+# Kill the cleaning up helper
+killall -13 "jamfHelper" >/dev/null 2>&1
+# Jamf Helper for app download+install
+jamfHelperDownloadInProgress
 
 exit 0
