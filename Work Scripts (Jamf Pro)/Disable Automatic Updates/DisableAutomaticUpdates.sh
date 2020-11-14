8e914bde18ea321fd583c48afc7227e196ffd3e4
pwalker1485 <pwalker1485@icloud.com>
2020-11-14 10:16:01 +0000

    amended to add a pre check for the Big Sur upgrade in ignored updates

--- Work Scripts (Jamf Pro)/Disable Automatic Updates/DisableAutomaticUpdates.sh
+++ Work Scripts (Jamf Pro)/Disable Automatic Updates/DisableAutomaticUpdates.sh
@@ -61,24 +61,10 @@ else
     exit 1
 fi
 # check the Big Sur upgrade has been ignored successfully
-postCheck=$(softwareupdate --ignore | grep -v "Ignored updates:" | sed -n 2p | xargs)
-if [[ "$postCheck" =~ "macOS Big Sur" ]]; then
-    echo "Big Sur upgrade via software update now blocked"
+if [[ "$(softwareupdate --ignore | grep -v "Ignored updates:")" =~ "macOS Big Sur" ]]; then
+    echo "Big Sur upgrade via software update set to ignore"
 else
     echo "Big Sur upgrade still available via software update, admin rights still required to complete the install!"
-fi
-}
-
-function removeBadgeAppIcon ()
-{
-if [[ "$loggedInUser" == "" ]] || [[ "$loggedInUser" == "root" ]]; then
-    echo "No one is home, cannot remove badge app icon"
-else
-    sysPrefsDockItem=$(su -l "$loggedInUser" -c "defaults read com.apple.dock" | grep "System%20Preferences.app")
-    if [[ "$sysPrefsDockItem" != "" ]]; then
-        sudo -u "$loggedInUser" defaults write com.apple.systempreferences AttentionPrefBundleIDs 0
-        echo "Badge app icon will be gone the next time the dock is relaunched"
-    fi
 fi
 }
 
@@ -89,7 +75,17 @@ fi
 # Confirm OS version is Catalina or earlier
 autoload is-at-least
 if ! is-at-least "$bigSur" "$osVersion"; then
-    echo "Mac running ${osVersion}, disabling auto updates and ignoring Big Sur..."
+    echo "Mac running ${osVersion}, checking ignored updates..."
+    # check to see if the Big Sur upgrade has already been ignored
+    if [[ "$(softwareupdate --ignore | grep -v "Ignored updates:")" =~ "macOS Big Sur" ]]; then
+        echo "Big Sur upgrade via software update already set to ignore"
+    else
+        echo "Big Sur upgrade currently available via software update"
+        # Ignore the Big Sur update
+        softwareupdate --reset-ignored >/dev/null 2>&1
+        softwareupdate --ignore "macOS Big Sur" >/dev/null 2>&1
+    fi
+    echo "Disabling automatic updates..."
     # Disable automatic background check for macOS software updates
     defaults write "$plistPath" AutomaticCheckEnabled -bool false
     # Disable automatic download of macOS software updates
@@ -100,13 +96,8 @@ if ! is-at-least "$bigSur" "$osVersion"; then
     defaults write "$plistPath" ConfigDataInstall -bool false
     # Disable automatic download and installation of automatic security updates
     defaults write "$plistPath" CriticalUpdateInstall -bool false
-    # Ignore the Big Sur update
-    softwareupdate --reset-ignored >/dev/null 2>&1
-    softwareupdate --ignore "macOS Big Sur" >/dev/null 2>&1
     # Check all settings have been applied successfully
     postCheck
-    # Remove the badge icon
-    removeBadgeAppIcon
 else
     echo "Mac running ${osVersion}, no changes required"
 fi
