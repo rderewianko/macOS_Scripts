5ff160516c8104b31514c195da164cf24e4f22fb
pwalker1485 <pwalker1485@icloud.com>
2021-01-29 23:48:42 +0000

    amended styling

--- Work Scripts (Jamf Pro)/Uninstall Applications/Apple/Uninstall_iWork_Garageband_and_iMovie_During_Enrolment.sh
+++ Work Scripts (Jamf Pro)/Uninstall Applications/Apple/Uninstall_iWork_Garageband_and_iMovie_During_Enrolment.sh
@@ -2,53 +2,49 @@
 
 ########################################################################
 #      Uninstall iWork, Garageband and iMovie During Enrolment         #
-################### Written by Phil Walker Feb 2010 ####################
+################### Written by Phil Walker Feb 2019 ####################
 ########################################################################
 
-#Garageband, iMovie, Keynote, Numbers and Pages are installed on all new Macs shipped by Apple
-#As standard the App Store is restricted on all Bauer Macs preventing these apps from being updated
-#VPP delivered versions of all of the below apps can be installed via Self Service if required
+# Garageband, iMovie, Keynote, Numbers and Pages are installed on all new Macs shipped by Apple
+# As standard the App Store is restricted on all Bauer Macs preventing these apps from being updated
+# VPP delivered versions of all of the below apps can be installed via Self Service if required
 
 ########################################################################
 #                            Variables                                 #
 ########################################################################
 
-#Non VPP Apple apps check
-appleApps=$(ls /Applications/ | grep -i "Garageband\|iMovie\|Keynote\|Numbers\|Pages" | awk 'END { print NR }')
-#Check to see if the DEPNotify app is open
-depNotify=$(ps aux | grep -v grep | grep "DEPNotify.app")
+# Non VPP Apple apps check
+masApps=$(find /Applications/ -maxdepth 1 | grep -i "Garageband\|iMovie\|Keynote\|Numbers\|Pages" | awk 'END {print NR}')
+# Check to see if the DEPNotify app is open
+depNotify=$(pgrep "DEPNotify")
 
 ########################################################################
 #                         Script starts here                           #
 ########################################################################
 
-#Confirm that the Mac is currently being enrolled
+# Confirm that the Mac is currently being enrolled
 if [[ "${depNotify}" != "" ]]; then
-  echo "DEP Notify process running, continuing..."
+    echo "DEP Notify process running, continuing..."
 else
-	echo "DEP Notify process not found, exiting..."
-  exit 0
+	  echo "DEP Notify process not found, exiting..."
+    exit 0
 fi
-
-if [[ "$appleApps" -ne "0" ]]; then
-
-  echo "Uninstalling iWork apps, Garageband and iMovie..."
+# If any Apple MAS apps are found, remove them
+if [[ "$masApps" -ne "0" ]]; then
+    echo "Uninstalling iWork apps, Garageband and iMovie..."
     rm -rf "/Applications/Garageband.app" 2>/dev/null
     rm -rf "/Applications/iMovie.app" 2>/dev/null
     rm -rf "/Applications/Keynote.app" 2>/dev/null
     rm -rf "/Applications/Numbers.app" 2>/dev/null
     rm -rf "/Applications/Pages.app" 2>/dev/null
-
-    appleApps=$(ls /Applications/ | grep -i "Garageband\|iMovie\|Keynote\|Numbers\|Pages" | awk 'END { print NR }')
-      if [[ "$appleApps" -eq "0" ]]; then
+    # re-populate variable
+    masApps=$(find /Applications/ -maxdepth 1 | grep -i "Garageband\|iMovie\|Keynote\|Numbers\|Pages" | awk 'END {print NR}')
+    if [[ "$masApps" -eq "0" ]]; then
         echo "iWork apps, Garageband and iMovie uninstalled successfully"
-      else
+    else
         echo "iWork apps, Garageband and iMovie uninstallation failed, manual removal required!"
-      fi
+    fi
 else
-
-
-  echo "iWork apps, Garageband and iMovie not found, nothing to do"
+    echo "iWork apps, Garageband and iMovie not found, nothing to do"
 fi
-
 exit 0
