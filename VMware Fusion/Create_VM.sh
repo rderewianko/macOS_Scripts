96326c869654d88c4e99e240684fa21aca005dd4
pwalker1485 <pwalker1485@icloud.com>
2021-01-29 23:04:04 +0000

    Amended styling

--- VMware Fusion/Create_VM.sh
+++ VMware Fusion/Create_VM.sh
@@ -30,28 +30,28 @@ DMGs=(/Users/"$loggedInUser"/Desktop/AutoDMG_Images/*)
 function checkDependencies ()
 {
 if [[ ! -d "$vmwareFusion" ]]; then
-  echo "VMware Fusion not installed, install VMware Fusion before running again. Exiting script..."
-  exit 0
+    echo "VMware Fusion not installed, install VMware Fusion before running again. Exiting script..."
+    exit 0
 else
-  if [[ ! -f "$vfuse" ]] && [[ ! -f "$qemu" ]]; then
-    echo "vfuse and qemu not installed"
-    echo "Install vfuse from https://github.com/chilcote/vfuse/releases"
-    echo "Install qemu via Homebrew, command: brew install qemu"
-    echo "Exiting script..."
-    exit 0
-  elif [[ -f "$vfuse" ]] && [[ ! -f "$qemu" ]]; then
-    echo "qemu not installed"
-    echo "Install qemu via Homebrew, command: brew install qemu"
-    echo "Exiting script..."
-    exit 0
-  elif [[ ! -f "$vfuse" ]] && [[ -f "$qemu" ]]; then
-    echo "vfuse not installed"
-    echo "Install vfuse from https://github.com/chilcote/vfuse/releases"
-    echo "Exiting script..."
-    exit 0
-  else
-    echo "Dependencies all installed"
-  fi
+    if [[ ! -f "$vfuse" ]] && [[ ! -f "$qemu" ]]; then
+        echo "vfuse and qemu not installed"
+        echo "Install vfuse from https://github.com/chilcote/vfuse/releases"
+        echo "Install qemu via Homebrew, command: brew install qemu"
+        echo "Exiting script..."
+        exit 0
+    elif [[ -f "$vfuse" ]] && [[ ! -f "$qemu" ]]; then
+        echo "qemu not installed"
+        echo "Install qemu via Homebrew, command: brew install qemu"
+        echo "Exiting script..."
+        exit 0
+    elif [[ ! -f "$vfuse" ]] && [[ -f "$qemu" ]]; then
+        echo "vfuse not installed"
+        echo "Install vfuse from https://github.com/chilcote/vfuse/releases"
+        echo "Exiting script..."
+        exit 0
+    else
+        echo "Dependencies all installed"
+    fi
 fi
 }
 
@@ -59,25 +59,24 @@ fi
 #                         Script starts here                           #
 ########################################################################
 
+# Check requirements are met
 checkDependencies
-
+# Prompt for a VM name
 read -rp "Enter the VM name: " vmName
-
+# Prompt for a serial number
 read -rp "Enter the serial number: " vmSerial
-
+# Prompt for a model number identifier
 read -rp "Enter the model number identifier: " vmModel
-
+# Check with OS DMG's are available
 read -rp "$(
         f=0
         for dmgname in "${DMGs[@]}" ; do
-                echo "$((++f)): $dmgname"
+            echo "$((++f)): $dmgname"
         done
-
         echo -ne 'Please select a DMG > '
 )" selection
-
 selectedDMG="${DMGs[$((selection-1))]}"
-
+# Confirm details
 echo "Check details entered are correct before continuing"
 echo
 echo "Virtual machine name entered: $vmName"
@@ -85,29 +84,26 @@ echo "Serial number entered: $vmSerial"
 echo "Model Identifier entered: $vmModel"
 echo "DMG selected: $selectedDMG"
 echo
-
 read -p "Are all details entered correct? (Yy/Nn) " -n 1 -r
 echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     echo "Details entered incorrect, exiting script..."
     exit 0
 fi
-
+# Use relevant method based on VMware Fusion version installed
 if [[ "$VMwareFusionVersion" -ge "11" ]]; then
-  echo "VMware Fusion 11 installed, qemu will need to be used"
-      if [[ $vmSerial == "" ]]; then
+    echo "VMware Fusion 11 installed, qemu will need to be used"
+    if [[ $vmSerial == "" ]]; then
         "$vfuse" -i "$selectedDMG" --hw-model "$vmModel" --use-qemu -n "$vmName"
-      else
+    else
         "$vfuse" -i "$selectedDMG" -s "$vmSerial" --hw-model "$vmModel" --use-qemu -n "$vmName"
-      fi
+    fi
 else
-  echo "VMware Fusion version 10 or previous"
-      if [[ $vmSerial == "" ]]; then
+    echo "VMware Fusion version 10 or previous"
+    if [[ $vmSerial == "" ]]; then
         "$vfuse" -i "$selectedDMG" --hw-model "$vmModel" -n "$vmName"
-      else
+    else
         "$vfuse" -i "$selectedDMG" -s "$vmSerial" --hw-model "$vmModel" -n "$vmName"
-      fi
+    fi
 fi
-
 exit 0
