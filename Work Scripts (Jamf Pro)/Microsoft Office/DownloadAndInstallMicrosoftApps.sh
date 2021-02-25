9a83044c7dfae96fbec35b5031dacd2a6fbad8d9
pwalker1485 <pwalker1485@icloud.com>
2021-02-25 21:44:30 +0000

    removed checksum check and added percentage output for DEPNotify

--- Work Scripts (Jamf Pro)/Microsoft Office/DownloadAndInstallMicrosoftApps.sh
+++ Work Scripts (Jamf Pro)/Microsoft Office/DownloadAndInstallMicrosoftApps.sh
@@ -2,11 +2,12 @@
 
 ########################################################################
 #              Download and Install Microsoft Applications             #
+################### written by Phil Walker Jan 2021 ####################
 ########################################################################
+# Edit Feb 2021 for download and install progress output to DEPNotify
 
 # Credit to Written William Smith (Professional Services Engineer @Jamf bill@talkingmoose.net
 # https://gist.github.com/talkingmoose/a16ca849416ce5ce89316bacd75fc91a
-# Edited by Phil Walker Jan 2021
 
 ########################################################################
 #                            Variables                                 #
@@ -32,51 +33,92 @@ linkID="$4"
 # 832978 - Skype for Business download
 # 869428 - Teams
 # 525134 - Word 2019 SKUless download
-# sha256 Checksum e.g. "67b1e8e036c575782b1c9188dd48fa94d9eabcb81947c8632fd4acac7b01644b"
-sha256Checksum="$5" # This is not required. Install will start with no value in this parameter.
 # Package Name
-pkgName="$6"
+pkgName="$5"
+# Friendly name for DEPNotify
+appName="$6"
+# Determinate level for DEPNotify
+determinateLevel="$7"
 ############ Variables for Jamf Pro Parameters - End ###################
 # Full fwlink URL
 fullURL="https://go.microsoft.com/fwlink/?linkid=$linkID"
+# DEPNotify process
+depNotify=$(pgrep "DEPNotify")
+# DEPNotify log
+logFile="/var/tmp/depnotify.log"
 
 ########################################################################
 #                         Script starts here                           #
 ########################################################################
 
 # Create a temporary working directory
-echo "Creating temporary directory for the download"
+/bin/echo "Creating temporary directory for the download"
 tempDirectory=$(/usr/bin/mktemp -d "/private/tmp/MicrosoftAppDownload.XXXXXX")
-
-# Download the installer package and name using the value found in parameter 6
-echo "Downloading package $pkgName.pkg"
-/usr/bin/curl --location --silent "$fullURL" -o "${tempDirectory}/${pkgName}.pkg"
-# Check if the download completed
-commandResult="$?"
-if [[ "$commandResult" -ne "0" ]]; then
-    echo "Failed to download the package, exiting..."
-    exit 1
-fi
-
-# Checksum the download
-downloadChecksum=$(/usr/bin/shasum -a 256 "${tempDirectory}/${pkgName}.pkg" | /usr/bin/awk '{ print $1 }')
-echo "Checksum for downloaded package: $downloadChecksum"
-# Install the package if checksum validates
-if [ "$sha256Checksum" = "$downloadChecksum" ] || [ "$sha256Checksum" = "" ]; then
-	echo "Checksum verified. Installing package $pkgName.pkg"
-	/usr/sbin/installer -pkg "${tempDirectory}/${pkgName}.pkg" -target /
+# Show download and install info in DEPNotify if the Mac is being provisioned
+if [[ "$depNotify" != "" ]]; then
+    /bin/echo "Mac is being provisioned, progress will be displayed in DEPNotify"
+    # Set determinate to Manual - Used to pause the status bar during the download/install process
+    /bin/echo "Command: DeterminateManual: ${determinateLevel}" >> "$logFile"
+    /bin/echo "Command: DeterminateManualStep: 1" >> "$logFile"
+    /bin/echo "Status: Downloading ${appName}..." >> "$logFile"
+    /bin/sleep 1
+    /bin/echo "Command: DeterminateManualStep: 1" >> "$logFile"
+    # Download the installer package and name using the value found in parameter 5
+    /bin/echo "Downloading ${appName} package..."
+    #/usr/bin/curl -L -# "$fullURL" -o "${tempDirectory}/${pkgName}.pkg" 2>&1 | while IFS= read -r -n1 char; do # bash version
+    /usr/bin/curl -L -# "$fullURL" -o "${tempDirectory}/${pkgName}.pkg" 2>&1 | while IFS= read -u 0 -sk 1 char; do
+        [[ $char =~ [0-9] ]] && keep=1;
+        [[ $char == % ]] && /bin/echo "Status: Downloading ${appName}... ${progress}%" >> "$logFile" && progress="" && keep=0;
+        [[ $keep == 1 ]] && progress="$progress$char";
+    done
+    # Check if the download completed
+    if [[ -e "${tempDirectory}/${pkgName}.pkg" ]]; then
+        /bin/echo "Successfully downloaded ${appName} package"
+    else
+        /bin/echo "Failed to download the package, exiting..."
+        exit 1
+    fi
+    /bin/echo "Command: DeterminateManualStep: 1" >> "$logFile"
+    /bin/echo "Status: Installing ${appName}..." >> "$logFile"
+    /bin/sleep 1
+    /bin/echo "Command: DeterminateManualStep: 1" >> "$logFile"
+    # Run installer in verboseR mode to give installer percentage and then output to DEPNotify
+    /bin/echo "Installing ${appName}..."
+	#/usr/sbin/installer -pkg "${tempDirectory}/${pkgName}.pkg" -target / -verboseR 2>&1 | while read -r -n1 char; do # bash version
+    /usr/sbin/installer -pkg "${tempDirectory}/${pkgName}.pkg" -target / -verboseR 2>&1 | while read -u 0 -sk 1 char; do
+        [[ $char == % ]] && keep=1;
+        [[ $char =~ [0-9] ]] && [[ $keep == 1 ]] && progress="$progress$char";
+        [[ $char == . ]] && [[ $keep == 1 ]] && /bin/echo "Status: Installing ${appName}... ${progress}%" >> "$logFile" && progress="" && keep=0;
+    done
+    /bin/echo "${appName} install complete"
+    /bin/echo "Command: DeterminateManualStep: 1" >> "$logFile"
+    /bin/echo "Status: ${appName} install complete" >> "$logFile"
+    /bin/sleep 1
+    /bin/echo "Command: DeterminateManualStep: 1" >> "$logFile"
+    # Set determinate back to auto
+    /bin/echo "Command: Determinate: ${determinateLevel}" >> "$logFile"
 else
-	echo "Checksum failed. Recalculate the SHA 256 checksum and try again. Or download may not be valid."
-	exit 1
+	/bin/echo "Mac not being provisioned, install silently"
+    # Download the installer package and name using the value found in parameter 5
+    /bin/echo "Downloading ${appName} package..."
+    /usr/bin/curl --location --silent "$fullURL" -o "${tempDirectory}/${pkgName}.pkg"
+    # Check if the download completed
+    commandResult="$?"
+    if [[ "$commandResult" -eq "0" ]]; then
+        /bin/echo "Successfully downloaded ${appName} package"
+    else
+        /bin/echo "Failed to download the package, exiting..."
+        exit 1
+    fi
+	/bin/echo "Installing ${appName}..."
+	/usr/sbin/installer -pkg "${tempDirectory}/${pkgName}.pkg" -target /
 fi
-
 # Remove the temporary working directory when done
 /bin/rm -Rf "$tempDirectory"
-echo "Deleting temporary directory '$tempDirectory' and its contents"
+/bin/echo "Deleting temporary directory ${tempDirectory} and its contents"
 if [[ ! -d "$tempDirectory" ]]; then
-    echo "Temporary directory deleted"
+    /bin/echo "Temporary directory deleted"
 else
-    echo "Failed to delete the temporary directory"
+    /bin/echo "Failed to delete the temporary directory"
 fi
-
 exit 0
