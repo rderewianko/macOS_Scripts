a17d93498ba896c7bbfa7fc01fa9ffec8e81ab68
pwalker1485 <pwalker1485@icloud.com>
2021-03-20 21:43:49 +0000

    switched to using encrypted creds

--- Work Scripts (Jamf Pro)/Jamf API/Cancel Failed MDM Commands/Cancel_Failed_Management_Commands.sh
+++ Work Scripts (Jamf Pro)/Jamf API/Cancel Failed MDM Commands/Cancel_Failed_Management_Commands.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/zsh
 
 ########################################################################
 #                 Cancel Failed Management Commands                    #
 ################ Written by Phil Walker November 2019 ##################
 ########################################################################
 # Edit Nov 2020
+# Edit Mar 2021
 
 ## API Username & Password
 ## Note: API account must have READ and UPDATE access to:
@@ -16,37 +17,50 @@
 #                            Variables                                 #
 ########################################################################
 
-# API creds for connection
-apiUser="API_User" #defined in the policy
-apiPass="API_Password" #defined in the policy
-# Jamf Pro URL (Leave off trailing slash)
-jamfProURL="https://yourjamfprourl/JSSResource" #defined in the policy
+# Encrypted credentials
+encryptedUsername="$4" # defined in the policy
+encryptedPassword="$5" # defined in the policy
+# Jamf Pro URL/JSSResource e.g. https://jamfprourl/JSSResource
+jamfProURL="$6" #defined in the policy
 # Get serial number
 serialNumber=$(system_profiler SPHardwareDataType | awk '/Serial Number/{print $4}')
+
+########################################################################
+#                            Functions                                 #
+########################################################################
+
+function decryptString () 
+{
+# Decrypt user credentials
+echo "${1}" | /usr/bin/openssl enc -aes256 -d -a -A -S "${2}" -k "${3}"
+}
 
 ########################################################################
 #                         Script starts here                           #
 ########################################################################
 
+# Decrypt the username and password
+apiUsername=$(decryptString "$encryptedUsername" 'Salt value' 'Passphrase value')
+apiPassword=$(decryptString "$encryptedPassword" 'Salt value' 'Passphrase value')
 # Check for failed Management Commands
-failedCommands=$(curl -sku "${apiUser}:${apiPass}" -H "accept: application/xml" "${jamfProURL}/computerhistory/serialnumber/${serialNumber}/subset/Commands" \
+failedCommands=$(curl -sku "${apiUsername}:${apiPassword}" -H "accept: application/xml" "${jamfProURL}/computerhistory/serialnumber/${serialNumber}/subset/Commands" \
 | xmllint --xpath '/computer_history/commands/failed' - | grep -i "command")
 # If failed commands are found, clear them all
 if [[ "$failedCommands" != "" ]]; then
     echo "Failed Management Commands found"
 	# Getting the computer ID
-	computerID=$(curl -sku "${apiUser}:${apiPass}" -H "accept: application/xml" "${jamfProURL}/computers/serialnumber/$serialNumber" \
-     | xmllint --xpath '/computer/general/id/text()' -)
+	computerID=$(curl -sku "${apiUsername}:${apiPassword}" -H "accept: application/xml" "${jamfProURL}/computers/serialnumber/$serialNumber" \
+    | xmllint --xpath '/computer/general/id/text()' -)
     untilCount="0"
     maxAttempts="5"
     until [[ "$failedCommands" == "" ]] || [[ "$untilCount" -eq "$maxAttempts" ]]; do
         (( untilCount++ ))
         echo "Attempt ${untilCount}: Removing failed Management Commands..."
         # Cancel all failed commands (run the command a maximum of 5 times)
-  	    curl -sku "${apiUser}:${apiPass}" -H "accept: application/xml" "${jamfProURL}/commandflush/computers/id/${computerID}/status/Failed" -X DELETE > /dev/null 2>&1
+  	    curl -sku "${apiUsername}:${apiPassword}" -H "accept: application/xml" "${jamfProURL}/commandflush/computers/id/${computerID}/status/Failed" -X DELETE > /dev/null 2>&1
         sleep 5
         # Re-populate variable
-		failedCommands=$(curl -sku "${apiUser}:${apiPass}" -H "accept: application/xml" "${jamfProURL}/computerhistory/serialnumber/${serialNumber}/subset/Commands" \
+		failedCommands=$(curl -sku "${apiUsername}:${apiPassword}" -H "accept: application/xml" "${jamfProURL}/computerhistory/serialnumber/${serialNumber}/subset/Commands" \
         | xmllint --xpath '/computer_history/commands/failed' - | grep -i "command")
     done
     if [[ "$failedCommands" == "" ]]; then
