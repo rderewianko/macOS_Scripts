f8515eeb282a5fba8ea1ce1d406c4d1726bc6079
pwalker1485 <pwalker1485@icloud.com>
2021-03-20 21:44:11 +0000

    switched to using encrypted creds

--- Work Scripts (Jamf Pro)/Jamf API/Bluetooth/Turn_Bluetooth_Off.sh
+++ Work Scripts (Jamf Pro)/Jamf API/Bluetooth/Turn_Bluetooth_Off.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/zsh
 
 ########################################################################
-#             Turn Bluetooth off via management command                #
+#                Turn Bluetooth off using MDM commands                 #
 ################## Written by Phil Walker July 2019 ####################
 ########################################################################
+# Edit Mar 2021
 
 ## API Username & Password
 ## Jamf Pro Server Objects
@@ -12,51 +13,55 @@
 ## Jamf Pro Server Actions
 ## • Send Computer Bluetooth Command
 
-##requires macOS 10.13.4 or later
+## requires macOS 10.13.4 or later
 
 ########################################################################
 #                            Variables                                 #
 ########################################################################
 
-#API creds for connection
-apiuser="$4" #defined in the policy
-apipass="$5" #defined in the policy
-#JSS URL (Leave off trailing slash)
-jssurl="$6" #defined in the policy
-#Get the logged in user
-loggedInUser=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
-#Get serial number
-serialNumber=$(ioreg -c IOPlatformExpertDevice -d 2 | awk -F\" '/IOPlatformSerialNumber/{print $(NF-1)}')
-#Bluetooth controller power status
+# Encrypted credentials
+encryptedUsername="$4" # defined in the policy
+encryptedPassword="$5" # defined in the policy
+# Jamf Pro URL (https://JamfProURL/JSSResource)
+jamfProURL="$6" # defined in the policy
+# Get serial number
+serialNumber=$(system_profiler SPHardwareDataType | awk '/Serial Number/{print $4}')
+# Bluetooth controller power status
 btPowerStatus=$(/usr/libexec/PlistBuddy -c "print ControllerPowerState" /Library/Preferences/com.apple.Bluetooth.plist)
+
+########################################################################
+#                            Functions                                 #
+########################################################################
+
+function decryptString () 
+{
+# Decrypt user credentials
+echo "${1}" | /usr/bin/openssl enc -aes256 -d -a -A -S "${2}" -k "${3}"
+}
 
 ########################################################################
 #                         Script starts here                           #
 ########################################################################
 
 if [[ "$btPowerStatus" == "0" ]] || [[ "$btPowerStatus" == "false" ]]; then
-
-  echo "Bluetooth already turned off, nothing to do"
-  exit 0
-
+    echo "Bluetooth already turned off, nothing to do"
+    exit 0
 else
-
-  echo "Turning Bluetooth off..."
-  #Getting the computer ID
-  ComputerID=$(curl -X GET "${jssurl}/JSSResource/computers/serialnumber/$serialNumber" -H "accept: application/xml" -sku "${apiuser}:${apipass}" | xmllint --format --xpath /computer/general/id - | awk -F '>|<' '{print $3}')
-
-  #Send Enable Bluetooth command
-  curl -X POST "${jssurl}/JSSResource/computercommands/command/SettingsDisableBluetooth/id/$ComputerID" -H "accept: application/xml" -sku "${apiuser}:${apipass}" > /dev/null 2>&1
-
+    # Decrypt the username and password
+    apiUsername=$(decryptString "$encryptedUsername" 'Salt value' 'Passphrase value')
+    apiPassword=$(decryptString "$encryptedPassword" 'Salt value' 'Passphrase value')
+    echo "Turning Bluetooth off..."
+    # Getting the computer ID
+    computerID=$(curl -sku "${apiUsername}:${apiPassword}" -H "accept: application/xml" "${jamfProURL}/computers/serialnumber/$serialNumber" \
+    | xmllint --xpath '/computer/general/id/text()' -)
+    # Send Disable Bluetooth command
+    curl -sku "${apiUsername}:${apiPassword}" -H "accept: application/xml" "${jamfProURL}/computercommands/command/SettingsDisableBluetooth/id/$computerID" -X POST >/dev/null 2>&1
 fi
-
-while true ; do
-  #Re-populate Bluetooth controller power status variable
-  btPowerStatus=$(/usr/libexec/PlistBuddy -c "print ControllerPowerState" /Library/Preferences/com.apple.Bluetooth.plist)
-   if [[ "$btPowerStatus" == "0" ]] || [[ "$btPowerStatus" == "false" ]]; then
-      echo "Bluetooth now off"
-      exit
-   fi
+# Confirm that Bluetooth is now off
+until [[ "$btPowerStatus" == "0" ]] || [[ "$btPowerStatus" == "false" ]]; do
+    sleep 1
+    # Re-populate Bluetooth controller power status variable
+    btPowerStatus=$(/usr/libexec/PlistBuddy -c "print ControllerPowerState" /Library/Preferences/com.apple.Bluetooth.plist)
 done
-
+echo "Bluetooth now off"
 exit 0
