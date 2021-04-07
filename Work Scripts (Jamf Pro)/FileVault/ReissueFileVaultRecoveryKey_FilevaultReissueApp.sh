3bfdc36bb24ba3ac66c2fa508590efd4a54a8e6f
pwalker1485 <pwalker1485@icloud.com>
2021-04-07 22:47:41 +0100

    Added FS helper and tidied up

--- Work Scripts (Jamf Pro)/FileVault/ReissueFileVaultRecoveryKey_FilevaultReissueApp.sh
+++ Work Scripts (Jamf Pro)/FileVault/ReissueFileVaultRecoveryKey_FilevaultReissueApp.sh
@@ -14,17 +14,30 @@ loggedInUser=$(stat -f %Su /dev/console)
 # Get the logged in users ID
 loggedInUserID=$(id -u "$loggedInUser")
 # Get the logged in users GUID
-userGUID=$(/usr/bin/dscl . -read /Users/$loggedInUser GeneratedUID | awk '{print $2}')
+userGUID=$(dscl . -read /Users/"$loggedInUser" GeneratedUID | awk '{print $2}')
 # Check if the logged in user is FileVault enabled already
-userFVEnabled=$(/usr/bin/fdesetup list | grep "$loggedInUser" | sed 's/.*,//g')
+userFVEnabled=$(fdesetup list | grep "$loggedInUser" | awk -F, '{print $2}')
+# Check the logged in users Secure Token status
+secureTokenStatus=$(sysadminctl -secureTokenStatus "$loggedInUser" 2>&1)
+if [[ "$secureTokenStatus" =~ "ENABLED" ]]; then
+	userSecureToken="Token"
+else
+	userSecureToken="NoToken"
+fi
 # FileVault status
-fileVaultStatus=$(/usr/bin/fdesetup status | grep "FileVault" | head -n 1)
+fileVaultStatus=$(fdesetup status | awk '/FileVault is/{print $3}' | tr -d .)
 # FileVault Reissue
 filevaultReissue="/private/var/tmp/Filevault Reissue.app"
 # FileVault Reissue app binary
 appBinary="/private/var/tmp/Filevault Reissue.app/Contents/MacOS/Filevault Reissue"
 # Bauer logo
 imagePath="/usr/local/BauerMediaGroup/FileVaultReissue/Bauer-Logo-icons1.png"
+# Jamf Helper
+jamfHelper="/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper"
+# Jamf Helper icon
+helperIcon="/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/FileVaultIcon.icns"
+# Temporary directory
+tempDir="/usr/local/BauerMediaGroup/FileVaultReissue"
 
 ########################################################################
 #                            Functions                                 #
@@ -40,30 +53,13 @@ else
 fi
 }
 
-function checkUserSecureToken ()
+function jamfHelperFullScreen ()
+# Fullscreen jamf Helper to try and force the user to take action
 {
-# Check the logged in users SecureToken status
-userSecureTokenStatus=$(/usr/sbin/sysadminctl -secureTokenStatus "$loggedInUser" 2>&1)
-if [[ "$userSecureTokenStatus" =~ "ENABLED" ]]; then
-	userSecureToken="Token"
-	echo "$loggedInUser has a SecureToken"
-else
-	userSecureToken="NoToken"
-	echo "$loggedInUser does NOT have a SecureToken"
-fi
-}
+"$jamfHelper" -windowType fs -icon "$helperIcon" -heading "Valid FileVault Recovery Key Not Found!" \
+-description "A valid FileVault Recovery Key is required to enable Bauer Technology to unlock your Mac should you experience any issues with your password
 
-function checkFileVault ()
-{
-# Check if FileVault has already been enabled
-if [[ "$fileVaultStatus" == "FileVault is On." ]]; then
-	userFVEnabled=$(fdesetup list | grep "$loggedInUser" | sed 's/.*,//g')
-	fvStatus="Enabled"
-	echo "FileVault already enabled"
-else
-	fvStatus="Disabled"
-	echo "FileVault is currently disabled"
-fi
+When prompted, please enter your username and password to issue a new FileVault Recovery Key" &
 }
 
 function cleanUp ()
@@ -82,28 +78,35 @@ fi
 #                         Script starts here                           #
 ########################################################################
 
+# Make sure the content directory is hidden
+chflags hidden "$tempDir"
 # Confirm that a user is logged in
 if [[ "$loggedInUser" == "root" ]] || [[ "$loggedInUser" == "" ]]; then
     echo "No one is home, exiting..."
+    cleanUp
     exit 0
 else
     # Check if the screen is locked
     lockScreenStatus=$(runAsUser /usr/bin/python -c 'import sys,Quartz; d=Quartz.CGSessionCopyCurrentDictionary(); print d' | grep "CGSSessionScreenIsLocked")
     # If the lockScreenStatus check is not empty, then the screensaver is on or the screen is locked
     if [[ -n "$lockScreenStatus" ]]; then
-        echo "${loggedInUser} is logged in, but the screen is locked, or the screensaver is active."
-        echo "Policy will run again tomorrow, exiting"
+        echo "${loggedInUser} is logged in, but the screen is locked or the screensaver is active."
+        echo "Policy will run again later, exiting"
         exit 0
     else
         echo "${loggedInUser} is logged in, and the screen is not locked. Continuing..."
-        checkUserSecureToken
-        checkFileVault
-        # Confirm that FileVault is on and the logged in user has a SecureToken and is FileVault enabled
-        # If the logged in user has a SecureToken, is a FileVault enabled user and FileVault has already been enabled, start the process
-        if [[ "$userSecureToken" == "Token" ]] && [[ "$userGUID" == "$userFVEnabled" ]] && [[ "$fvStatus" == "Enabled" ]]; then
+        # Confirm that FileVault is on, the logged in user has a SecureToken and is FileVault enabled
+        # If the logged in user has a Secure Token, is a FileVault enabled user and FileVault has already been enabled, start the process
+        if [[ "$userSecureToken" == "Token" ]] && [[ "$userGUID" == "$userFVEnabled" ]] && [[ "$fileVaultStatus" == "On" ]]; then
             echo "$loggedInUser has a SecureToken, is a FileVault enabled user and FileVault is enabled"
             # Confirm the content is there
             if [[ -d "$filevaultReissue" ]] && [[ -f "$imagePath" ]]; then
+                # Display full screen jamf Helper
+                echo "Displaying fullscreen Jamf Helper message"
+                jamfHelperFullScreen
+                sleep 20
+                # Kill the full screen jamf Helper
+                killall -13 jamfHelper 2>/dev/null
                 echo "Opening Filevault Reissue..."
                 # App must be run as root or another admin user
                 "$appBinary"
