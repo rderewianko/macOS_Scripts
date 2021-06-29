3f0d09524336ba833cc135be4f69baebd6387aa1
pwalker1485 <pwalker1485@icloud.com>
2021-06-29 23:31:20 +0100

    removed pre-check

--- Work Scripts (Jamf Pro)/ARD/ConfigureARD.sh
+++ Work Scripts (Jamf Pro)/ARD/ConfigureARD.sh
@@ -9,8 +9,6 @@
 #                            Variables                                 #
 ########################################################################
 
-# Check local admin is configured for ARD
-ardConfigForAdmin=$(dscl . -list /Users naprivs | awk '{print $1}' | grep "admin")
 # ARD Kickstart
 ardKickstart="/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart"
 
@@ -18,24 +16,20 @@ ardKickstart="/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Content
 #                         Script starts here                           #
 ########################################################################
 
-if [[ "$ardConfigForAdmin" != "admin" ]]; then
-    echo "Configuring ARD..."
-    # Activate and enable for specified users only
-    "$ardKickstart" -activate -configure -allowAccessFor -specifiedUsers
-    # Enable for admin and root and give them all privs
-    "$ardKickstart" -configure -access -on -privs -all -users admin,root
-    # Enable the menubar icon so users can see if they are being controlled
-    "$ardKickstart" -configure -clientopts -setmenuextra -menuextra yes
-    # Restart the client to ensure that all config changes are in place
-    "$ardKickstart" -restart -agent -console -menu
-    # re-populate variable post changes
-    ardConfigForAdmin=$(dscl . -list /Users naprivs | awk '{print $1}' | grep "admin")
-    if [[ "$ardConfigForAdmin" == "admin" ]]; then
-        echo "ARD configured successfully"
-    else
-        echo "Failed to configure ARD for admin!"
-    fi
+echo "Configuring ARD..."
+# Activate and enable for specified users only
+"$ardKickstart" -activate -configure -allowAccessFor -specifiedUsers &>/dev/null
+# Enable for admin and root and give them all privs
+"$ardKickstart" -configure -access -on -privs -all -users admin,root &>/dev/null
+# Enable the menubar icon so users can see if they are being controlled
+"$ardKickstart" -configure -clientopts -setmenuextra -menuextra yes &>/dev/null
+# Restart the client to ensure that all config changes are in place
+"$ardKickstart" -restart -agent -console -menu &>/dev/null
+# Check changes
+ardConfigForAdmin=$(dscl . -list /Users naprivs | awk '{print $1}' | grep "admin")
+if [[ "$ardConfigForAdmin" == "admin" ]]; then
+    echo "ARD configured successfully"
 else
-    echo "ARD is already configured, nothing to do"
+    echo "Failed to configure ARD for admin!"
 fi
 exit 0
