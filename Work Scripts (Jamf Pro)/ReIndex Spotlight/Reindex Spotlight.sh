63b5dde4fcf041fcc0c1292aa179ab1e8fc60ed5
pwalker1485 <pwalker1485@icloud.com>
2021-06-30 23:18:47 +0100

    Updated for Catalina or later

--- Work Scripts (Jamf Pro)/ReIndex Spotlight/Reindex Spotlight.sh
+++ Work Scripts (Jamf Pro)/ReIndex Spotlight/Reindex Spotlight.sh
@@ -1,66 +1,60 @@
-#!/bin/bash
+#!/bin/zsh
+
+########################################################################
+#                         Reindex Spotlight                            #
+################## Written by Phil Walker Jan 2020 #####################
+########################################################################
 
 # This script is designed to fix Spotlight indexing issues
 # by removing the existing Spotlight index and forcing Spotlight
 # to create a new search index.
-# Edited by Phil Walker Jan 2020
+# Edited by Phil Walker June 2021 for macOS Catalina or later only
 
 ########################################################################
 #                            Variables                                 #
 ########################################################################
 
-#Metadata server launch daemon PID
+# Metadata server launch daemon process ID
 launchDPID1=$(launchctl list | grep "com.apple.metadata.mds" | grep -v "index\|scan\|spindump" | awk '{print $1}')
+# Spotlight database
+spotlightDB="/System/Volumes/Data/.Spotlight-V100"
 
 ########################################################################
 #                         Script starts here                           #
 ########################################################################
 
 # Turn Spotlight indexing off
-
-/usr/bin/mdutil -i off / >/dev/null 2>&1
-
+echo "Disabling indexing..."
+mdutil -i off / &>/dev/null
 # Display Spotlight status
-
-/usr/bin/mdutil -s / | sed -n '2p' | xargs | tr -d '.'
-
-# Delete the Spotlight folder on the root level of the boot volume
-
-/bin/rm -rf /.Spotlight*
-
-# Confirm Spotlight folder has been deleted
-
-if [[ ! -d "/.Spotlight-V100" ]]; then
-  echo "Spotlight folder deleted successfully"
+mdutil -s / | sed -n '2p' | xargs | tr -d '.'
+# Delete the Spotlight database on the root level of the boot volume
+echo "Deleting the current Spotlight database..."
+if [[ -d "$spotlightDB" ]]; then
+    rm -rf "$spotlightDB"
+    # Confirm Spotlight database has been deleted
+    if [[ ! -d "$spotlightDB" ]]; then
+        echo "Spotlight database deleted"
+    else
+        echo "Spotlight database still present!"
+    fi
 else
-  echo "Spotlight folder still present"
+    echo "Spotlight database not found!"
 fi
-
 # Turn Spotlight indexing on and erase the current local store
-
-/usr/bin/mdutil -i on / >/dev/null 2>&1
-
-# Erase the current local store
-
-/usr/bin/mdutil -E / >/dev/null 2>&1
-
-# Display Spotlight status
-
+echo "Enabling indexing..."
+mdutil -i on / &>/dev/null
+mdutil -E / &>/dev/null
 sleep 2
-
-/usr/bin/mdutil -s / | sed -n '2p' | xargs | tr -d '.'
-
+# Display Spotlight status
+mdutil -s / | sed -n '2p' | xargs | tr -d '.'
 # Restart Spotlight service
-
 launchctl kickstart -k system/com.apple.metadata.mds
-
 launchDPID2=$(launchctl list | grep "com.apple.metadata.mds" | grep -v "index\|scan\|spindump" | awk '{print $1}')
-
-#Wait for the spotlight service to be restarted before continuing
+# Wait for the spotlight service to be restarted before continuing
 while [[ "$launchDPID1" -eq "$launchDPID2" ]]; do
-  echo "Spotlight service being restarted..."
-  sleep 1;
+    echo "Spotlight service being restarted..."
+    sleep 1;
 done
 echo "Spotlight service restarted"
-
 exit 0
